18fe55e1a32fc65bd4ea79cfe390e7ab2fc5429b
light <seseifunc16@gmail.com>
2022-10-02 16:26:44 +0900

    .

--- abc/271/c.cpp
+++ abc/271/c.cpp
@@ -17,18 +17,6 @@ using namespace std;
 using ll = long long;
 
 
-void print() { cout << '\n'; }
-template<class T> void print(const T& value) { cout << value << '\n'; }
-template<class T, class... A> void print(const T& first, const A&... tail) { cout << first << " "; print(tail...); }
-template<class... A> void print(const A&... tail) { print(tail...); }
-template<class T> void print(vector<T>& vec) { for (const auto& a : vec) { cout << a << " "; } print(); }
-template<class T> void print(vector<vector<T>>& vec2d) { for (auto& vec : vec2d) { print(vec); } }
-void print(vector<string>& grid) { for (const auto& row : grid) print(row); }
-template<class T, class U> void print(const map<T, U>& mp) { for (const auto& [x, y] : mp) { print(x, y); } }
-template<class T> void print(set<T>& st) { for (const auto& a : st) { cout << a << " "; } print(); }
-void print(vector<pair<ll, ll>>& vp) { for (auto [x, y] : vp) print(x, y); }
-
-
 int main() {
   ll n;
   cin >> n;
@@ -36,25 +24,28 @@ int main() {
   vector<ll> a(n);
   rep(i, n) cin >> a[i];
 
-  vector<ll> seen(n+1);
-  ll cnt = 0;
-  rep(i, n) {
-    if (a[i] > n) ++cnt;
-    else if (not seen[a[i]]) seen[a[i]] = true;
-    else ++cnt;
+  ll ok = 0;
+  ll ng = 3e5+1;
+  while (abs(ok-ng) > 1) {
+    ll mid = min(ok, ng) + abs(ok-ng)/2;
+  
+    auto check = [&]() {
+      set<ll> less_mid;
+
+      rep(i, n) {
+        if (a[i] <= mid) less_mid.emplace(a[i]);
+      }
+
+      ll rem = less_mid.size();
+      ll need = mid - rem;
+      ll extra = n - rem;
+      return extra/2 >= need;
+    };
+  
+    if (check()) ok = mid;
+    else ng = mid;
   }
 
-  ll i = 1;
-  while (1) {
-    if (seen[i]) ++i;
-    else if (cnt >= 2) {
-      cnt -= 2;
-      seen[i] = true;
-      ++i;
-    }
-    else break;
-  }
-
-  cout << i-1 << '\n';
+  cout << ok << '\n';
   return 0;
 }
