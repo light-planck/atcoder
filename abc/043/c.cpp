3a20d39577a3f9a7b6d1f88d49bf1c24a7fe6b89
light <seseifunc16@gmail.com>
2022-11-09 21:58:05 +0900

    .

--- abc/043/c.cpp
+++ abc/043/c.cpp
@@ -5,6 +5,21 @@ using ll = long long;
 
 
 int main() {
-  
+  auto chmin = [](auto& a, auto b) { if (a > b) a = b; };
+
+  ll n;
+  cin >> n;
+
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll ans = 9e18;
+  for (ll i = -100; i <= 100; ++i) {
+    ll cnt = 0;
+    rep(j, n) cnt += (i-a[j]) * (i-a[j]);
+    chmin(ans, cnt);
+  }
+
+  cout << ans << '\n';
   return 0;
 }
