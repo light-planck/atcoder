0d3e4cb8013a9a1b091e2f78d0b93de1c20700a7
light <seseifunc16@gmail.com>
2022-11-09 21:33:22 +0900

    .

--- abc/045/b.cpp
+++ abc/045/b.cpp
@@ -1,23 +1,27 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll n = 3;
+
+  vector<deque<char>> deq(n);
+  rep(i, n) {
+    string s;
+    cin >> s;
+    for (auto c : s) deq[i].emplace_back(c);
+  }
+
+  ll now = 0;
+  while (true) {
+    if (deq[now].empty()) {
+      cout << char(now+'A') << '\n';
+      break;
+    }
+    char c = deq[now].front(); deq[now].pop_front();
+    now = c - 'a';
+  }
   return 0;
 }
