c9f152880df7a87674796a821111a353081bbba2
light <seseifunc16@gmail.com>
2022-11-09 22:10:54 +0900

    .

--- abc/042/b.cpp
+++ abc/042/b.cpp
@@ -1,23 +1,21 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll n, l;
+  cin >> n >> l;
+
+  vector<string> s(n);
+  rep(i, n) {
+    cin >> s[i];
+  }
+  sort(s.begin(), s.end());
+
+  string ans = "";
+  rep(i, n) ans += s[i];
+  cout << ans << '\n';
   return 0;
 }
