07cc87b31a652b41d6961a99dd7b37ed60b59877
light <seseifunc16@gmail.com>
2022-11-30 23:15:01 +0900

    .

--- abc/278/d.cpp
+++ abc/278/d.cpp
@@ -5,6 +5,51 @@ using ll = long long;
 
 
 int main() {
-  
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+
+  map<ll, ll> add;
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    add[i] = a;
+  }
+
+  ll q;
+  cin >> q;
+
+  ll base = 0;
+
+  while (q--) {
+    ll type;
+    cin >> type;
+
+    if (type == 1) {
+      ll x;
+      cin >> x;
+
+      base = x;
+      add.clear();
+    }
+
+    if (type == 2) {
+      ll i, x;
+      cin >> i >> x;
+      --i;
+
+      add[i] += x;
+    }
+
+    if (type == 3) {
+      ll i;
+      cin >> i;
+      --i;
+
+      cout << base+add[i] << '\n';
+    }
+  }
   return 0;
 }
