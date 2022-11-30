d74f4d3e7de447c5dd44dd047b3b7b556bdcaeb5
light <seseifunc16@gmail.com>
2022-11-30 19:02:46 +0900

    .

--- abc/278/c.cpp
+++ abc/278/c.cpp
@@ -5,6 +5,28 @@ using ll = long long;
 
 
 int main() {
-  
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n, q;
+  cin >> n >> q;
+
+  map<pair<ll, ll>, bool> follow;
+
+  while (q--) {
+    ll type, a, b;
+    cin >> type >> a >> b;
+
+    if (type == 1) {
+      follow[{a, b}] = true;
+    }
+    if (type == 2) {
+      follow[{a, b}] = false;
+    }
+    if (type == 3) {
+      if (follow[{a, b}] and follow[{b, a}]) cout << "Yes" << '\n';
+      else cout << "No" << '\n';
+    }
+  }
   return 0;
 }
