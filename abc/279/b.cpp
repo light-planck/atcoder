1c8820204afdb7aedf11addb75794e8ff7eb5f79
light <seseifunc16@gmail.com>
2022-12-01 18:34:42 +0900

    .

--- abc/279/b.cpp
+++ abc/279/b.cpp
@@ -5,6 +5,29 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  string t;
+  cin >> t;
+
+  ll n = s.size();
+  ll m = t.size();
+
+  if (n < m) {
+    cout << "No" << '\n';
+    return 0;
+  }
+
+  rep(i, n) {
+    if (i+m > n) break;
+
+    if (s.substr(i, m) == t) {
+      cout << "Yes" << '\n';
+      return 0;
+    }
+  }
+
+  cout << "No" << '\n';
   return 0;
 }
