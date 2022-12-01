223c53ef58c705044d48fcf5c7a4076739411162
light <seseifunc16@gmail.com>
2022-12-01 20:35:17 +0900

    .

--- abc/279/d.cpp
+++ abc/279/d.cpp
@@ -2,9 +2,34 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+using ldouble = long double;
 
 
 int main() {
+  ll a, b;
+  cin >> a >> b;
+
+  auto f = [&](ll x) {
+    return b*x + a/pow(x+1, 0.5);
+  };
+
+  auto df = [&](ll x) {
+    return b - a/(2*pow(x+1, 3.0/2));
+  };
+
+  ll ng = -1;
+  ll ok = 9e18;
+  while (abs(ok-ng) > 1) {
+    ll mid = min(ok, ng) + abs(ok-ng)/2;
   
+    auto check = [&]() {
+      return df(mid) >= 0;
+    };
+  
+    if (check()) ok = mid;
+    else ng = mid;
+  }
+
+  printf("%.10f\n", f(ok));
   return 0;
 }
