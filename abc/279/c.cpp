64e3d5c9ea16439ed59444acf017fe4df7201529
light <seseifunc16@gmail.com>
2022-12-01 19:02:07 +0900

    .

--- abc/279/c.cpp
+++ abc/279/c.cpp
@@ -5,6 +5,29 @@ using ll = long long;
 
 
 int main() {
-  
+  ll h, w;
+  cin >> h >> w;
+
+  vector<string> s(h);
+  rep(i, h) cin >> s[i];
+
+  vector<string> t(h);
+  rep(i, h) cin >> t[i];
+
+  auto convert = [&](vector<string> grid) {
+    vector<string> res;
+    rep(j, w) {
+      string col = "";
+      rep(i, h) {
+        col += grid[i][j];
+      }
+      res.emplace_back(col);
+    }
+    sort(res.begin(), res.end());
+    return res;
+  };
+
+  if (convert(s) == convert(t)) cout << "Yes" << '\n';
+  else cout << "No" << '\n';
   return 0;
 }
