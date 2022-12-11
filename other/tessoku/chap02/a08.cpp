c85ea360f407d413eef655ce78d7a2b3f97788f7
light <seseifunc16@gmail.com>
2022-12-11 17:56:34 +0900

    .

---
+++ other/tessoku/chap02/a08.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+using namespace std;
+using ll = long long;
+
+
+int main() {
+  ll h, w;
+  cin >> h >> w;
+
+  vector x(h, vector<ll>(w));
+  rep(i, h) rep(j, w) cin >> x[i][j];
+
+  vector s(h+1, vector<ll>(w+1));
+  rep(i, h) rep(j, w) {
+    s[i+1][j+1] = s[i+1][j] + x[i][j];
+  }
+  rep(j, w) rep(i, h) {
+    s[i+1][j+1] += s[i][j+1];
+  }
+
+  ll q;
+  cin >> q;
+
+  while (q--) {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    --a; --b; --c; --d;
+
+    ll ans = s[c+1][d+1] - s[c+1][b] - s[a][d+1] + s[a][b];
+    cout << ans << '\n';
+  }
+  return 0;
+}
