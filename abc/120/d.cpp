3aeb71a8443e962050635eb62627dc99b3bbb8a8
light <seseifunc16@gmail.com>
2022-12-21 21:37:12 +0900

    .

--- abc/120/d.cpp
+++ abc/120/d.cpp
@@ -2,9 +2,15 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+#include <atcoder/all>
+using namespace atcoder;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  vector<pair> edge;
+  rep
   return 0;
 }
