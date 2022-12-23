8039fe718933db9dad301d35a1de236cac83e89b
light <seseifunc16@gmail.com>
2022-12-23 22:33:57 +0900

    .

--- abc/120/d.cpp
+++ abc/120/d.cpp
@@ -2,7 +2,7 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
-#include <atcoder/all>
+#include <atcoder/dsu>
 using namespace atcoder;
 
 
@@ -11,6 +11,29 @@ int main() {
   cin >> n >> m;
 
   vector<pair<ll, ll>> edge;
-  rep
+  rep(_, m) {
+    ll a, b;
+    cin >> a >> b;
+    --a; --b;
+    edge.emplace_back(a, b);
+  }
+  reverse(edge.begin(), edge.end());
+
+  dsu uf(n);
+  vector<ll> ans(m);
+  ans[0] = n * (n-1) / 2;
+
+  rep(i, m-1) {
+    auto [a, b] = edge[i];
+    ans[i+1] = ans[i];
+
+    if (uf.same(a, b)) continue;
+
+    ans[i+1] -= uf.size(a) * uf.size(b);
+    uf.merge(a, b);
+  }
+
+  reverse(ans.begin(), ans.end());
+  for (auto num : ans) cout << num << '\n';
   return 0;
 }
