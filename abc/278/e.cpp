20c1f11b31bbda49a7673d968e636d7d30072487
light <seseifunc16@gmail.com>
2022-12-28 22:02:43 +0900

    .

--- abc/278/e.cpp
+++ abc/278/e.cpp
@@ -4,8 +4,6 @@ using namespace std;
 using ll = long long;
 
 
-ll s[301][301][301];
-
 int main() {
   ll H, W, n, h, w;
   cin >> H >> W >> n >> h >> w;
@@ -13,8 +11,30 @@ int main() {
   vector a(H, vector<ll>(W));
   rep(i, H) rep(j, W) cin >> a[i][j];
 
-  rep(i, H) rep(j, W) {
-    s[i+1][j+1][a[i][j]] = s[i+1][j][a[i][j]] + 1;
+  ll i = 0; ll j = 0;
+  ll dir = 1;
+  map<ll, ll> now;
+  while (true) {
+    auto ok = [&]() {
+      if (i < 0 or i+h >= H) return false;
+      if (j < 0 or j+w >= W) return false;
+      return true;
+    };
+    
+    if (not ok()) break;
+    
+    j += dir;
+    if (dir == 1 and j == w) {
+      // del: a[i][j] ~ a[i][w]
+      // add: a[i+1][j] ~ a[i+1][w]
+
+      dir = -1;
+      ++i;
+    }
+    if (dir == -1 and j == 0) {
+      dir = 1;
+      ++i;
+    }
   }
   return 0;
 }
