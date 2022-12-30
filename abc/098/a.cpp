609ddcbcb9893746221ec9db5ff9d3e993557a9f
light <seseifunc16@gmail.com>
2022-12-31 00:10:12 +0900

    .

--- abc/098/a.cpp
+++ abc/098/a.cpp
@@ -1,23 +1,14 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll a, b;
+  cin >> a >> b;
+
+  ll ans = max({a+b, a-b, a*b});
+  cout << ans << '\n';
   return 0;
 }
