7408b82639ee51080ab76a396d79ffade80e09af
light <seseifunc16@gmail.com>
2022-12-30 22:35:16 +0900

    .

--- abc/106/a.cpp
+++ abc/106/a.cpp
@@ -1,23 +1,13 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll a, b;
+  cin >> a >> b;
+
+  cout << a*b - a - b + 1 << '\n';
   return 0;
 }
