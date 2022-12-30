bbbe77fc6c7f587383f277594ff4a1c40a3ee44e
light <seseifunc16@gmail.com>
2022-12-30 16:54:09 +0900

    .

--- abc/262/d.cpp
+++ abc/262/d.cpp
@@ -13,30 +13,32 @@ int main() {
   vector<ll> a(n);
   rep(i, n) cin >> a[i];
 
-  // dp[i][j]: i = sum%n, j = 選んだ個数のときの場合の数
-  vector dp(n, vector<ll>(n+1));
-  dp[0][0] = 1;
-
-  rep(i, n) {
-    vector prev(n, vector<ll>(n+1));
-    swap(prev, dp);
-
-    for (ll j = 0; j < n; ++j) {
-      for (ll k = 0; k <= n; ++k) {
-        // 選ばない
-        dp[j][k] += prev[j][k];
-
-        // 選ぶ
-        if (k+1 <= n) dp[(j+a[i]) % (i+1)][k+1] += prev[j][k];
-      }
-    }
-  }
-
   using mint = modint998244353;
   mint ans = 0;
-  rep(j, n) for (ll k = 1; k <= n; ++k) {
-    if (dp[j][k] % k == 0) ans += (mint)dp[j][k];
+  
+  for (ll l = 1; l <= n; ++l) {
+    // dp[j][k]: j個選んだときの総和%lがkのときの場合の数
+    vector dp(l+1, vector<mint>(l));
+    dp[0][0] = 1;
+
+    rep(i, n) {
+      vector prev(l+1, vector<mint>(l));
+      swap(prev, dp);
+
+      for (ll j = 0; j <= l; ++j) {
+        for (ll k = 0; k < l; ++k) {
+          // 選ばない
+          dp[j][k] += prev[j][k];
+
+          // 選ぶ
+          if (j+1 <= l) dp[j+1][(k+a[i])%l] += prev[j][k];
+        }
+      }
+    }
+
+    ans += dp[l][0];
   }
+
   cout << ans.val() << '\n';
   return 0;
 }
