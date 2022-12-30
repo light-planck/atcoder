89667cbe6885183717701db4de0055efebf12c7d
light <seseifunc16@gmail.com>
2022-12-30 22:49:23 +0900

    .

--- abc/104/a.cpp
+++ abc/104/a.cpp
@@ -5,6 +5,11 @@ using ll = long long;
 
 
 int main() {
-  
+  ll r;
+  cin >> r;
+
+  if (r < 1200) cout << "ABC" << '\n';
+  else if (r < 2800) cout << "ARC" << '\n';
+  else cout << "AGC" << '\n';
   return 0;
 }
