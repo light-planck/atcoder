2db3d573cad01a6b567c5c939a26d52349b13ce4
light <seseifunc16@gmail.com>
2022-12-31 20:01:16 +0900

    .

--- abc/105/b.cpp
+++ abc/105/b.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define all(x) (x).begin(),(x).end()
 using ll = long long;
-const int inf = 1e9;
-const int mod = 1e9+7;
 
-bool eight(int n) {
-  int cnt = 0;
-  for (int i = 1; i <= n; i++) {
-    if (n % i == 0) cnt++;
-  }
-  if (cnt == 8) return true;
-  else return false;
-}
 
 int main() {
-  int n;
+  ll n;
   cin >> n;
-  int ans = 0;
-  for (int i = 1; i <= n; i = i + 2) {
-    if (eight(i)) ans++;
+
+  for (ll i = 0; i <= n; ++i) {
+    for (ll j = 0; j <= n; ++j) {
+      if (4*i + 7*j == n) {
+        cout << "Yes" << '\n';
+        return 0;
+      }
+    }
   }
-  cout << ans << endl;
+  cout << "No" << '\n';
   return 0;
 }
