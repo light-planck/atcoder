8dde5ff9e8cc13804fbfc36d24d170fae8171bc9
light <seseifunc16@gmail.com>
2022-12-31 16:03:28 +0900

    .

---
+++ abc/001/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+using namespace std;
+using ll = long long;
+
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  cout << a-b << '\n';
+  return 0;
+}
