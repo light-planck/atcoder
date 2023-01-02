12885d4b7860c9a29d9dd5c04ea23b7b51e74a3a
light <seseifunc16@gmail.com>
2023-01-02 16:10:00 +0900

    .

--- abc/044/c.cpp
+++ abc/044/c.cpp
@@ -5,16 +5,40 @@ using ll = long long;
 
 
 int main() {
-  // ll n, a;
-  // cin >> n >> a;
+  ll n, a;
+  cin >> n >> a;
 
-  // rep(l, n) {
-  //   ll lim = n * 50;
-  //   vector dp(n+1, vector<vector<ll>>(l+1, vector<ll>(lim)));
-  // }
+  vector<ll> x(n);
+  rep(i, n) cin >> x[i];
 
-  vector dp(1, vector<vector<ll>>(1, vector<ll>(1)));
-  dp[0][0][0] = 100;
-  cout << dp[0][0][0] << '\n';
+  ll ans = 0;
+  for (ll select = 1; select <= n; ++select) {
+    ll max_sum = select * 50;
+
+    // dp[k][l]: j番目まで見てk個選んだときの総和がlの場合の数
+    vector dp(select+1, vector<ll>(max_sum+1));
+    dp[0][0] = 1;
+
+    rep(j, n) {
+      vector prev(select+1, vector<ll>(max_sum+1));
+      swap(prev, dp);
+
+      for (ll choose = 0; choose <= select; ++choose) {
+        for (ll sum = 0; sum <= max_sum; ++sum) {
+          // 選ばない
+          dp[choose][sum] += prev[choose][sum];
+
+          // 選ぶ
+          if (choose+1 <= select and sum+x[j] <= max_sum) {
+            dp[choose+1][sum+x[j]] += prev[choose][sum];
+          }
+        }
+      }
+    }
+
+    ans += dp[select][select*a];
+  }
+
+  cout << ans << '\n';
   return 0;
 }
