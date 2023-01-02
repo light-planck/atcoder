46f521c08242580e5d2b8b94c0ecafe0d5575e52
light <seseifunc16@gmail.com>
2023-01-02 13:12:34 +0900

    .

--- abc/046/c.cpp
+++ abc/046/c.cpp
@@ -5,6 +5,23 @@ using ll = long long;
 
 
 int main() {
-  
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+
+  pair<ll, ll> prev = {1, 1};
+  rep(i, n) {
+    ll t, a;
+    cin >> t >> a;
+
+    auto [pt, pa] = prev;
+    auto round = [](ll a, ll b) { return (a + b - 1) / b; };
+    ll k = max(round(pt, t), round(pa, a));
+    prev = {k*t, k*a};
+  }
+
+  cout << prev.first + prev.second << '\n';
   return 0;
 }
