40a93e5e2bdf16f3f3d5fbdf31976fab96f2dc1c
light <seseifunc16@gmail.com>
2023-01-02 14:42:20 +0900

    .

--- abc/044/c.cpp
+++ abc/044/c.cpp
@@ -5,6 +5,16 @@ using ll = long long;
 
 
 int main() {
-  
+  // ll n, a;
+  // cin >> n >> a;
+
+  // rep(l, n) {
+  //   ll lim = n * 50;
+  //   vector dp(n+1, vector<vector<ll>>(l+1, vector<ll>(lim)));
+  // }
+
+  vector dp(1, vector<vector<ll>>(1, vector<ll>(1)));
+  dp[0][0][0] = 100;
+  cout << dp[0][0][0] << '\n';
   return 0;
 }
