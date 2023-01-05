842181e4cb4d9a0150ba05ee37db72a652bd63be
light <seseifunc16@gmail.com>
2023-01-05 14:45:28 +0900

    .

--- abc/103/d.cpp
+++ abc/103/d.cpp
@@ -1,23 +1,30 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  vector<pair<ll, ll>> lr;
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    lr.emplace_back(a, b);
+  }
+  sort(lr.begin(), lr.end(), [](pair<ll, ll> a, pair<ll, ll>b ){ return a.second < b.second; });
+
+  ll ans = 0;
+  ll stop = 0;
+  for (auto [left, right] : lr) {
+    if (left >= stop) {
+      stop = right;
+      ++ans;
+    }
+  }
+
+  cout << ans << '\n';
   return 0;
 }
