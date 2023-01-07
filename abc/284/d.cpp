e956e33f2e2ff6ae640d112f96825e13079443e0
light <seseifunc16@gmail.com>
2023-01-07 22:59:01 +0900

    .

--- abc/284/d.cpp
+++ abc/284/d.cpp
@@ -11,40 +11,28 @@ int main() {
   ll t;
   cin >> t;
 
-  auto Sqrt = [](ll p) -> ll {
-    for (ll i = 2; i*i <= p; ++i) {
-      if (p%i == 0) {
-        return p/i;
-      }
-    }
-    return p;
-  };
-
   while (t--) {
     ll n;
     cin >> n;
 
-    auto solve = [&]() {
-      ll p = 1; ll q = 0;
+    ll p = 1; ll q = 0;
 
-      for (ll i = 2; i*i*i <= n; ++i) {
-        if (n%i == 0) {
-          if ((n/i) % i == 0) {
-            p = i;
-            q = n / i / i;
-          }
-          else {
-            q = i;
-            p = n / q;
-            p = Sqrt(p);
-          }
+    for (ll i = 2; i*i*i <= n; ++i) {
+      if (n%i == 0) {
+        if ((n/i) % i == 0) {
+          p = i;
+          q = n / i / i;
         }
+        else {
+          q = i;
+          p = n / q;
+          p = sqrt(p);
+        }
+        break;
       }
+    }
 
-      cout << p << " " << q << '\n';
-    };
-
-    solve();
+    cout << p << " " << q << '\n';
   }
   return 0;
 }
