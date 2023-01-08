ec7bd5a5aff353ef81531e3e5e6ed62fc03869e9
light <seseifunc16@gmail.com>
2023-01-08 15:42:36 +0900

    .

--- abc/103/b.cpp
+++ abc/103/b.cpp
@@ -1,23 +1,27 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  string t;
+  cin >> t;
+
+  ll n = s.size();
+  rep(i, n) {
+    string tmp = s.substr(0, n-1);
+    s = s.back() + tmp;
+
+    if (s == t) {
+      cout << "Yes" << '\n';
+      return 0;
+    }
+  }
+
+  cout << "No" << '\n';
   return 0;
 }
