1b5531b3ad52599d2fb7aaa7a3b5da8c269c4f60
light <seseifunc16@gmail.com>
2023-01-09 23:13:45 +0900

    .

--- abc/096/d.cpp
+++ abc/096/d.cpp
@@ -1,23 +1,31 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
+// 2, 3, ... , sqrt(n)まで調べればよい
+bool is_prime(long long x) {
+  if (x < 2) return false;
+
+  for (long long i = 2; i * i <= x; ++i) {
+    if (x % i == 0) return false;
+  }
+
+  return true;
+}
+
+
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<ll> primes;
+  for (ll i = 2; i <= 55555; ++i) {
+    if (is_prime(i) and i%5 == 1) primes.emplace_back(i);
+  }
+
+  rep(i, n) cout << primes[i] << ' ';
+  cout << '\n';
   return 0;
 }
