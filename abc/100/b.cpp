cf4d5a2d033075a556928f76d68a43eea8c59f91
light <seseifunc16@gmail.com>
2023-01-09 13:36:15 +0900

    .

--- abc/100/b.cpp
+++ abc/100/b.cpp
@@ -5,6 +5,14 @@ using ll = long long;
 
 
 int main() {
-  
+  ll d, n;
+  cin >> d >> n;
+
+  ll ans = 0;
+  if (n == 100) ++n;
+
+  auto Pow = [](ll x, ll n) { ll res = 1; while (n > 0) { if (n & 1) res *= x; x *= x; n >>= 1; } return res; };
+  ans = Pow(100, d) * n;
+  cout << ans << '\n';
   return 0;
 }
