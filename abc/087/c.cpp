74b75520dedd9383cc0f253afbc0bc50ac5fa180
light <seseifunc16@gmail.com>
2023-01-12 15:37:31 +0900

    .

--- abc/087/c.cpp
+++ abc/087/c.cpp
@@ -5,6 +5,29 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector a(2, vector<ll>(n));
+  rep(i, 2) rep(j, n) cin >> a[i][j];
+
+  const vector<pair<ll, ll>> dij = {{0, -1}, {-1, 0}};
+
+  auto sum = a;
+  rep(i, 2) rep(j, n) {
+    auto chmax = [](auto& a, auto b) { if (a < b) a = b; };
+
+    ll add = 0;
+    for (auto [di, dj] : dij) {
+      ll pi = i + di; ll pj = j + dj;
+      if (pi < 0 or pi >= 2) continue;
+      if (pj < 0 or pj >= n) continue;
+      chmax(add, sum[pi][pj]);
+    }
+
+    sum[i][j] += add;
+  }
+
+  cout << sum[1][n-1] << '\n';
   return 0;
 }
