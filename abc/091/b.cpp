bee3773c971f08dedde97d1b8bd987e6f4f199f4
light <seseifunc16@gmail.com>
2023-01-14 03:10:04 +0900

    .

--- abc/091/b.cpp
+++ abc/091/b.cpp
@@ -5,6 +5,30 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  map<string, ll> score;
+  rep(i, n) {
+    string s;
+    cin >> s;
+    ++score[s];
+  }
+
+  ll m;
+  cin >> m;
+  rep(i, m) {
+    string t;
+    cin >> t;
+    --score[t];
+  }
+
+  ll ans = 0;
+  for (auto [s, cnt] : score) {
+    auto chmax = [](auto& a, auto b) { if (a < b) a = b; };
+    chmax(ans, cnt);
+  }
+
+  cout << ans << '\n';
   return 0;
 }
