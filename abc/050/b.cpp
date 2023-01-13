684ceb3d2f31ca50c53950c7ec63e0ea3d2b513d
light <seseifunc16@gmail.com>
2023-01-14 01:40:30 +0900

    .

--- abc/050/b.cpp
+++ abc/050/b.cpp
@@ -1,23 +1,30 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+
+  vector<ll> t(n);
+  rep(i, n) cin >> t[i];
+
+  ll m;
+  cin >> m;
+
+  ll sum = accumulate(t.begin(), t.end(), 0ll);
+  rep(i, m) {
+    ll p, x;
+    cin >> p >> x;
+    --p;
+    
+    ll ans = sum - t[p] + x;
+    cout << ans << '\n';
+  }
   return 0;
 }
