34793402d2c57e6c612533215b9e7f9befc80ac0
light <seseifunc16@gmail.com>
2023-01-14 02:18:01 +0900

    .

--- abc/098/b.cpp
+++ abc/098/b.cpp
@@ -1,23 +1,34 @@
-#include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  string s;
+  cin >> s;
+
+  ll ans = 0;
+  rep(i, n-2) {
+    string t = s.substr(0, i+1);
+    string u = s.substr(i+1);
+
+    set<char> x;
+    for (auto c : t) x.emplace(c);
+
+    set<char> y;
+    for (auto c : u) y.emplace(c);
+
+    ll cnt = 0;
+    for (auto c : x) if (y.count(c)) ++cnt;
+
+    auto chmax = [](auto& a, auto b) { if (a < b) a = b; };
+    chmax(ans, cnt);
+  }
+
+  cout << ans << '\n';
   return 0;
 }
