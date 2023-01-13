9e724038d902fc3602e2b07640a45c1245b53cfd
light <seseifunc16@gmail.com>
2023-01-14 01:53:44 +0900

    .

--- abc/056/b.cpp
+++ abc/056/b.cpp
@@ -5,6 +5,11 @@ using ll = long long;
 
 
 int main() {
-  
+  ll w, a, b;
+  cin >> w >> a >> b;
+
+  if (b > a+w) cout << b-a-w << '\n';
+  else if (b+w < a) cout << a-b-w << '\n'; 
+  else cout << 0 << '\n';
   return 0;
 }
