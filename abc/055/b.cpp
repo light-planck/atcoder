ca64a952ce28efc558cbed0bfe599939903d7042
light <seseifunc16@gmail.com>
2023-01-14 01:49:18 +0900

    .

--- abc/055/b.cpp
+++ abc/055/b.cpp
@@ -2,9 +2,18 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+#include <atcoder/modint>
+using namespace atcoder;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  using mint = modint1000000007;
+  mint ans = 1;
+  rep(i, n) ans *= i + 1;
+
+  cout << ans.val() << '\n';
   return 0;
 }
