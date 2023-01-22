a7e4b4052c1af39dccf1234fd0569c0cc4abef7c
light <seseifunc16@gmail.com>
2023-01-22 20:33:10 +0900

    .

--- abc/074/b.cpp
+++ abc/074/b.cpp
@@ -5,6 +5,19 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  ll k;
+  cin >> k;
+
+  vector<ll> x(n);
+  rep(i, n) cin >> x[i];
+
+  ll ans = 0;
+  rep(i, n) {
+    ans += min(2*abs(x[i]-k), 2*abs(x[i]));
+  }
+  cout << ans << '\n';
   return 0;
 }
