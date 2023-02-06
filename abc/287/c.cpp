110fa44a0b6af30b303bacb13e7065ee3efd9f4c
light <seseifunc16@gmail.com>
2023-02-06 14:15:12 +0900

    .

--- abc/287/c.cpp
+++ abc/287/c.cpp
@@ -2,9 +2,33 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+#include <atcoder/dsu>
+using namespace atcoder;
 
 
 int main() {
+  ll n, m;
+  cin >> n >> m;
+
+  vector<ll> cnt(n);
+  rep(i, m) {
+    ll u, v;
+    cin >> u >> v;
+    --u; --v;
+
+    ++cnt[u];
+    ++cnt[v];
+  }
+
+  bool ok = true;
+  ll edge = 0;
+  rep(i, n) {
+    if (cnt[i] == 1) ++edge;
+    else if (cnt[i] != 2) ok = false;
+  }
+  if (edge != 2) ok = false;
   
+  if (ok) cout << "Yes" << '\n';
+  else cout << "No" << '\n'; 
   return 0;
 }
