67fcee7cc003704c56e006ab807d5cd09eb64511
light <seseifunc16@gmail.com>
2023-02-06 12:21:38 +0900

    .

--- abc/287/b.cpp
+++ abc/287/b.cpp
@@ -5,6 +5,25 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  vector<string> t(m);
+  rep(i, m) cin >> t[i];
+
+  ll ans = 0;
+  rep(i, n) {
+    rep(j, m) {
+      if (s[i].substr(3) == t[j]) {
+        ++ans;
+        break;
+      }
+    }
+  }
+
+  cout << ans << '\n';
   return 0;
 }
