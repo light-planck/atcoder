b0dda58a4f2c43a86e9a9035f09fba22f3d2f604
light <seseifunc16@gmail.com>
2023-02-17 21:49:20 +0900

    .

--- abc/289/c.cpp
+++ abc/289/c.cpp
@@ -5,6 +5,34 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  vector<set<ll>> st(m);
+  rep(i, m) {
+    ll c;
+    cin >> c;
+    rep(_, c) {
+      ll a;
+      cin >> a;
+      st[i].emplace(a);
+    }
+  }
+
+  ll ans = 0;
+  for (ll bit = 0; bit < (1<<m); ++bit) {
+    set<ll> s;
+    rep(i, m) {
+      if ((bit>>i) & 1) {
+        for (auto x : st[i]) {
+          s.emplace(x);
+        }
+      }
+    }
+    
+    if ((ll)s.size() == n) ++ans;
+  }
+
+  cout << ans << '\n';
   return 0;
 }
