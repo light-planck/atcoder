5585f095ca657d2ad7a60e8237529d4e9b4c8d39
light <seseifunc16@gmail.com>
2023-03-13 03:29:54 +0900

    .

---
+++ arc/158/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+using namespace std;
+using ll = long long;
+
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll t;
+  cin >> t;
+
+  while (t--) {
+    ll n = 3;
+    vector<ll> x(n);
+    rep(i, n) cin >> x[i];
+
+    
+  }
+  return 0;
+}
