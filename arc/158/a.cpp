b22d62dede11b98621c074ee36ef6565c5eaa404
light <seseifunc16@gmail.com>
2023-03-15 22:20:04 +0900

    .

--- arc/158/a.cpp
+++ arc/158/a.cpp
@@ -15,8 +15,21 @@ int main() {
     ll n = 3;
     vector<ll> x(n);
     rep(i, n) cin >> x[i];
+    ll a = accumulate(x.begin(), x.end(), 0ll) / 3;
+    bool ok = true;
 
-    
+    if ((x[0]+x[1]+x[2]) %3 != 0) ok = false;
+    if (x[0]%2!=x[1]%2 or x[1]%2!=x[2]%2 or x[2]%2!=x[0]%2) ok = false;
+    if (a%2 != x[0]%2) ok = false;
+
+    if (not ok) {
+      cout << -1 << '\n';
+      continue;
+    }
+
+    ll d = 0;
+    rep(i, n) d += abs(x[i] - a);
+    cout << d/4 << '\n';
   }
   return 0;
 }
