0f7486241f815cb1454fc9dbab6e59e3b02cff20
light <seseifunc16@gmail.com>
2023-03-17 00:46:17 +0900

    .

--- abc/289/d.cpp
+++ abc/289/d.cpp
@@ -5,6 +5,36 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll m;
+  cin >> m;
+
+  vector<ll> b(m);
+  rep(i, m) cin >> b[i];
+
+  ll x;
+  cin >> x;
+
+  vector<ll> dp(x+1);
+  dp[0] = 1;
+  rep(i, m) dp[b[i]] = -1;
+
+  rep(i, x+1) {
+    if (dp[i] != 1) continue;
+    
+    rep(j, n) {
+      ll k = i + a[j];
+      if (k > x or dp[k] == -1) continue;
+      dp[k] = true;
+    }
+  }
+
+  if (dp[x]) cout << "Yes" << '\n';
+  else cout << "No" << '\n';
   return 0;
 }
