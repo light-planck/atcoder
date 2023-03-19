c2f909177fc3be181d4fe8f2ff7b32fe623c843a
light <seseifunc16@gmail.com>
2023-03-19 17:51:47 +0900

    .

--- abc/287/d.cpp
+++ abc/287/d.cpp
@@ -5,6 +5,34 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  string t;
+  cin >> t;
+
+  ll n = t.size();
+  ll m = s.size();
+
+  vector<bool> pre(n+1);
+  pre[0] = true;
+  rep(i, n) {
+    if (not pre[i]) continue;
+
+    if ((s[i]==t[i]) or (s[i]=='?' or t[i]=='?')) pre[i + 1] = true;
+  }
+
+  vector<bool> suf(n+1);
+  suf[0] = true;
+  rep(i, n) {
+    if (not suf[i]) continue;
+
+    if ((s[m-1-i]==t[n-1-i]) or (s[m-1-i]=='?' or t[n-1-i]=='?')) suf[i + 1] = true;
+  }
+
+  rep(x, n+1) {
+    if (pre[x] and suf[n-x]) cout << "Yes" << '\n';
+    else cout << "No" << '\n';
+  }
   return 0;
 }
