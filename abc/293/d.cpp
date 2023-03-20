c76a2950b6130d0d8b9645042b3fe4653fb4998c
light <seseifunc16@gmail.com>
2023-03-20 15:00:55 +0900

    .

--- abc/293/d.cpp
+++ abc/293/d.cpp
@@ -2,9 +2,27 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+#include <atcoder/dsu>
+using namespace atcoder;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  dsu uf(n);
+  ll ring = 0;
+  rep(i, m) {
+    ll a, c;
+    char b, d;
+    cin >> a >> b >> c >> d;
+    --a; --c;
+    if (uf.same(a, c)) ++ring;
+    else uf.merge(a, c);
+  }
+
+  ll ans = 0;
+  rep(i, n) if (uf.leader(i)==i) ++ans;
+  cout << ring << " " << ans-ring << '\n';
   return 0;
 }
