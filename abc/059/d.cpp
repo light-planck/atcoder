4fbab575ec4c479670e3a826f3712050281cf383
light <seseifunc16@gmail.com>
2023-03-21 22:04:17 +0900

    .

--- abc/059/d.cpp
+++ abc/059/d.cpp
@@ -5,6 +5,23 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  ll n = s.size();
+  map<char, vector<ll>> pos;
+  rep(i, n) pos[s[i]].emplace_back(i);
+
+  for (auto [c, vi] : pos) {
+    ll m = vi.size();
+    if (m <= 1) continue;
+
+    rep(j, m-1) if (vi[j+1]-vi[j] <= 2) {
+      cout << vi[j]+1 << " " << vi[j+1]+1 << '\n';
+      return 0;
+    }
+  }
+
+  cout << -1 << " " << -1 << '\n';
   return 0;
 }
