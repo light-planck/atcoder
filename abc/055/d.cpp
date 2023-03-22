e4c2d0bea435ecae62657bd3f9db5489917ea82f
light <seseifunc16@gmail.com>
2023-03-23 01:40:29 +0900

    .

--- abc/055/d.cpp
+++ abc/055/d.cpp
@@ -5,6 +5,52 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  string s;
+  cin >> s;
+
+  rep(a, 2) rep(b, 2) {
+    vector<ll> animals(n, -1);
+    animals[0] = a;
+    animals[n-1] = b;
+    bool ok = true;
+    rep(i, n) {
+      ll prev = (i-1+n) % n;
+      ll next = (i+1) % n;
+      if (animals[i] == 1) {
+        if (s[i] == 'o') {
+          if (animals[next] == -1) animals[next] = animals[prev];
+          else if (animals[next] != animals[prev]) ok = false;
+        }
+        else {
+          if (animals[next] == -1) animals[next] = animals[prev] ^ 1;
+          else if (animals[next] == animals[prev]) ok = false;
+        }
+      }
+      else {
+        if (s[i] == 'o') {
+          if (animals[next] == -1) animals[next] = animals[prev] ^ 1;
+          else if (animals[next] == animals[prev]) ok = false;
+        }
+        else {
+          if (animals[next] == -1) animals[next] = animals[prev];
+          else if (animals[next] != animals[prev]) ok = false;
+        }
+      }
+    }
+
+    if (not ok) continue;
+
+    for (auto d : animals) {
+      if (d == 1) cout << 'S';
+      else cout << 'W';
+    }
+    cout << '\n';
+    return 0;
+  }
+
+  cout << -1 << '\n';
   return 0;
 }
