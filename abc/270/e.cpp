a886260b7cb57cd56e99d7479670f4974e3bc0b8
light <seseifunc16@gmail.com>
2023-03-24 22:54:24 +0900

    .

--- abc/270/e.cpp
+++ abc/270/e.cpp
@@ -1,10 +1,38 @@
-#include <bits/stdc++.h>
-#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
-using namespace std;
-using ll = long long;
+#include <iostream>
+#include <vector>
 
 
 int main() {
-  
+  long long n, k;
+  std::cin >> n >> k;
+
+  std::vector<long long> a(n);
+  for (auto& ai : a) std::cin >> ai;
+
+  long long ok = 0;
+  long long ng = 1e12 + 1;
+  while (abs(ok-ng) > 1) {
+    long long mid = (ok+ng) / 2;
+    long long eat = 0;
+    for (auto ai : a) {
+      eat += std::min(ai, mid);
+    }
+    if (eat <= k) ok = mid;
+    else ng = mid;
+  }
+
+  for (auto& ai : a) {
+    k -= std::min(ai, ok);
+    ai = std::max(0ll, ai-ok);
+  }
+  for (auto& ai : a) {
+    if (k <= 0) break;
+    if (ai == 0) continue;
+    --ai;
+    --k;
+  }
+
+  for (auto ai : a) std::cout << ai << ' ';
+  std::cout << '\n';
   return 0;
 }
