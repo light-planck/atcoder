78772935c1ab7d04e8ae93ec683e6d767d96ef5a
light <seseifunc16@gmail.com>
2023-03-27 18:16:35 +0900

    .

--- abc/169/e.cpp
+++ abc/169/e.cpp
@@ -5,6 +5,7 @@ using ll = long long;
 
 
 double avg(vector<ll>& a) {
+  sort(a.begin(), a.end());
   ll n = a.size();
   if (n%2 == 1) return a[n/2];
   else return (a[n/2]+a[n/2 - 1]) / 2.0;
@@ -18,17 +19,13 @@ int main() {
   vector a(n, 0ll), b(n, 0ll);
   rep(i, n) cin >> a[i] >> b[i];
 
-  auto c = a;
-  sort(c.begin(), c.end());
-  double m = avg(c);
-
-  auto d = b;
-  sort(d.begin(), d.end());
-  double M = avg(d);
+  double m = avg(a);
+  double M = avg(b);
 
   ll ans = 0;
   if (n%2 == 0) ans = ((M-m) / 0.5) + 1;
   else ans = M - m + 1;
+  
   cout << ans << '\n';
   return 0;
 }
