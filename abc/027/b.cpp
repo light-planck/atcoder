5ea4f9743b2670c1a5b298f23e6ddd6a6defad31
light <seseifunc16@gmail.com>
2023-03-31 05:49:48 +0900

    .

--- abc/027/b.cpp
+++ abc/027/b.cpp
@@ -5,6 +5,27 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector a(n, 0ll);
+  rep(i, n) cin >> a[i];
+
+  ll sum = accumulate(a.begin(), a.end(), 0ll);
+  if (sum%n != 0) {
+    cout << -1 << '\n';
+    return 0;
+  }
+
+  ll ans = 0;
+  rep(i, n-1) {
+    ll left = 0;
+    for (ll l = 0; l <= i; ++l) left += a[l];
+    ll right = 0;
+    for (ll r = i+1; r < n; ++r) right += a[r];
+    if (left != (i+1)*sum/n or right != (n-i-1)*sum/n) ++ans;
+  }
+
+  cout << ans << '\n';
   return 0;
 }
