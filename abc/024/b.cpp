bf758b9dc95cd0115cec8ca379c06e8495e1194b
light <seseifunc16@gmail.com>
2023-03-31 23:38:10 +0900

    .

--- abc/024/b.cpp
+++ abc/024/b.cpp
@@ -5,6 +5,19 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n, t;
+  cin >> n >> t;
+
+  vector a(n, 0ll);
+  rep(i, n) cin >> a[i];
+
+  ll ans = 0;
+  rep(i, n-1) {
+    if (a[i+1]-a[i] > t) ans += t;
+    else ans += a[i+1] - a[i];
+  }
+  ans += t;
+
+  cout << ans << '\n';
   return 0;
 }
