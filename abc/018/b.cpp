25c33adedfd567d0779c1bb0491cf870c330f2b0
light <seseifunc16@gmail.com>
2023-04-02 18:05:11 +0900

    .

--- abc/018/b.cpp
+++ abc/018/b.cpp
@@ -4,7 +4,33 @@ using namespace std;
 using ll = long long;
 
 
+string Slice(string& s, ll l, ll r) {
+  if (r-l < 0 or r-l > (ll)s.size()) return "";
+  return s.substr(l, r-l);
+}
+
+
 int main() {
-  
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  string s;
+  cin >> s;
+
+  ll n;
+  cin >> n;
+
+  rep(i, n) {
+    ll l, r;
+    cin >> l >> r;
+    --l; --r;
+
+    string rev = Slice(s, l, r+1);
+    reverse(rev.begin(), rev.end());
+
+    s = Slice(s, 0, l) + rev + Slice(s, r+1, (ll)s.size());
+  }
+
+  cout << s << '\n';
   return 0;
 }
