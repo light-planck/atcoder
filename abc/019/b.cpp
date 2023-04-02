12b0410bf24a092e2c8852e631e473b68263cb50
light <seseifunc16@gmail.com>
2023-04-02 17:38:41 +0900

    .

--- abc/019/b.cpp
+++ abc/019/b.cpp
@@ -5,6 +5,29 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  vector<pair<char, ll>> encoded;
+  ll cnt = 0;
+  char prev = '.';
+  for (char c : s) {
+    if (c == prev) ++cnt;
+    else {
+      if (prev != '.') {
+        encoded.emplace_back(prev, cnt);
+      }
+
+      cnt = 1;
+      prev = c;
+    }
+  }
+  encoded.emplace_back(prev, cnt);
+
+  string ans = "";
+  for (auto [c, cnt] : encoded) {
+    ans += c + to_string(cnt);
+  }
+  cout << ans << '\n';
   return 0;
 }
