feedc86a30f31870884f14b600afc195e63327c6
light <seseifunc16@gmail.com>
2023-04-02 09:46:00 +0900

    .

--- abc/296/b.cpp
+++ abc/296/b.cpp
@@ -9,10 +9,11 @@ int main() {
 
   vector<string> s(n);
   rep(i, n) cin >> s[i];
+  reverse(s.begin(), s.end());
 
   rep(i, n) rep(j, n) {
     if (s[i][j] == '*') {
-      cout << (char)('a'+j) << 1+7-i << endl;
+      cout << (char)('a'+j) << i+1 << endl;
     }
   }
   return 0;
