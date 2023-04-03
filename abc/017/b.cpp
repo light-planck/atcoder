abc270b8203e83543d663de5c0e2df25264e2d6a
light <seseifunc16@gmail.com>
2023-04-03 19:41:40 +0900

    .

--- abc/017/b.cpp
+++ abc/017/b.cpp
@@ -5,6 +5,22 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+
+  reverse(s.begin(), s.end());
+  ll n = s.size();
+
+  rep(i, n) {
+    char c = s[i];
+    if (c == 'o' or c == 'k' or c == 'u') continue;
+    if (i+1<n and s[i]=='h' and s[i+1]=='c') continue;
+    if (i-1>=0 and s[i]=='c' and s[i-1]=='h') continue;
+    
+    cout << "NO" << endl;
+    return 0;
+  }
+
+  cout << "YES" << endl;
   return 0;
 }
