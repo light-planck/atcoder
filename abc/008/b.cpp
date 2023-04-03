713b1f82a658475b65554a00c8abc0d490285baa
light <seseifunc16@gmail.com>
2023-04-03 21:39:17 +0900

    .

--- abc/008/b.cpp
+++ abc/008/b.cpp
@@ -5,6 +5,25 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  map<string, ll> cnt;
+  rep(i, n) {
+    string s;
+    cin >> s;
+    ++cnt[s];
+  }
+
+  ll ans = 0;
+  string name = "";
+  for (auto [s, x] : cnt) {
+    if (x > ans) {
+      ans = x;
+      name = s;
+    }
+  }
+
+  cout << name << '\n';
   return 0;
 }
