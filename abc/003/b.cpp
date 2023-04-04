a2e3ff90a7ffc41e633eb15d1342e5858fbd7ff8
light <seseifunc16@gmail.com>
2023-04-04 10:12:29 +0900

    .

--- abc/003/b.cpp
+++ abc/003/b.cpp
@@ -5,6 +5,25 @@ using ll = long long;
 
 
 int main() {
-  
+  string s, t;
+  cin >> s >> t;
+
+  ll n = s.size();
+  set<char> st = {'a', 't', 'c', 'o', 'd', 'e', 'r'};
+  rep(i, n) {
+    bool ng = false;
+    if (t[i] == '@') swap(s[i], t[i]);
+    if (s[i] == '@' and t[i] != '@' and not st.count(t[i])) {
+      ng = true;
+    }
+    else if (s[i] != t[i]) ng = true;
+
+    if (ng) {
+      cout << "You will lose" << '\n';
+      return 0;
+    }
+  }
+
+  cout << "You can win" << '\n';
   return 0;
 }
