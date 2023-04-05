552ead7747664cdd839be7434fb37ee9e6546cbe
light <seseifunc16@gmail.com>
2023-04-05 22:52:38 +0900

    .

--- abc/041/c.cpp
+++ abc/041/c.cpp
@@ -5,6 +5,18 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector a(n, 0ll);
+  rep(i, n) cin >> a[i];
+
+  map<ll, ll> idx;
+  rep(i, n) idx[a[i]] = i + 1;
+
+  auto b = a;
+  sort(b.rbegin(), b.rend());
+
+  rep(i, n) cout << idx[b[i]] << '\n';
   return 0;
 }
