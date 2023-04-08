34fb3af63861534b4acecd29de3758ee4d2b0011
light <seseifunc16@gmail.com>
2023-04-09 01:00:55 +0900

    .

--- abc/029/c.cpp
+++ abc/029/c.cpp
@@ -5,6 +5,25 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<string> ans;
+
+  auto dfs = [&](auto self, string s) -> void {
+    if ((ll)s.size() == n) {
+      ans.emplace_back(s);
+      return;
+    }
+
+    rep(i, 3) {
+      char c = 'a' + i;
+      self(self, s+c);
+    }
+  };
+  dfs(dfs, "");
+
+  sort(ans.begin(), ans.end());
+  for (auto x : ans) cout << x << '\n';
   return 0;
 }
