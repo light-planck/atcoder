cbc9a67453583a75dfc6419b71421e09f1f6f3f7
light <seseifunc16@gmail.com>
2023-04-09 01:18:44 +0900

    .

--- abc/039/c.cpp
+++ abc/039/c.cpp
@@ -5,6 +5,24 @@ using ll = long long;
 
 
 int main() {
-  
+  string s;
+  cin >> s;
+  s = s + s;
+
+  string t = "WBWBWWBWBWBW";
+  vector<string> melody = {"Do", "Re", "Mi", "Fa", "So", "La", "Si", "Do"};
+
+  ll step = 0;
+  rep(i, t.size()+1) {
+    string u = s.substr(i, t.size());
+    cout << u << '\n';
+    cout << t << '\n';
+    cout << "___________" << '\n';
+    if (u == t) {
+      cout << melody[step] << '\n';
+      return 0;
+    }
+    ++step;
+  }
   return 0;
 }
