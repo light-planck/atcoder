ca0bc3d5d7814fc1edc533aef6d3ccf705945212
light <seseifunc16@gmail.com>
2023-04-12 17:42:16 +0900

    .

--- abc/038/c.cpp
+++ abc/038/c.cpp
@@ -5,6 +5,25 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll ans = 0;
+  ll r = 1;
+  rep(l, n) {
+    if (l == r) ++r;
+    
+    while (r < n and (a[r]>a[r-1])) {
+      ++r;
+    }
+
+    ans += r - l;
+    if (r == l) ++r;
+  }
+
+  cout << ans << '\n';
   return 0;
 }
