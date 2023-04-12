593c309a3e56963b6fe62df0a0b3282978258c12
light <seseifunc16@gmail.com>
2023-04-12 22:03:29 +0900

    .

--- abc/032/c.cpp
+++ abc/032/c.cpp
@@ -5,6 +5,35 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n, k;
+  cin >> n >> k;
+
+  vector<ll> s(n);
+  rep(i, n) cin >> s[i];
+
+  rep(i, n) {
+    if (s[i] == 0) {
+      cout << n << '\n';
+      return 0;
+    }
+  }
+
+  ll ans = 0;
+  deque<ll> deq;
+  ll prod = 1;
+
+  rep(i, n) {
+    deq.emplace_back(s[i]);
+    prod *= s[i];
+
+    while (not deq.empty() and prod > k) {
+      prod /= deq[0];
+      deq.pop_front();
+    }
+
+    ans = max(ans, (ll)deq.size());
+  }
+
+  cout << ans << '\n';
   return 0;
 }
