818acafbce9ebe23900230b09a1958f7a6f2c08c
light <seseifunc16@gmail.com>
2023-04-12 13:43:02 +0900

    .

--- abc/095/d.cpp
+++ abc/095/d.cpp
@@ -8,43 +8,30 @@ int main() {
   ll n, c;
   cin >> n >> c;
 
-  vector<ll> x(n), v(n);
-  rep(i, n) cin >> x[i] >> v[i];
-
-  // 最大値を保持
-  vector<ll> max_value(n+1);
-  for (ll i = 0; i < n; ++i) max_value[i+1] = max(max_value[i], max_value[i]+v[i]-x[i]);
-
-  // valueの累積和
-  vector<ll> sum_v(n+1);
-  rep(i, n) sum_v[i+1] += sum_v[i] + v[i];
+  // 原点Oを初項として挿入
+  vector<ll> x(n+1), v(n+1);
+  rep(i, n) cin >> x[i+1] >> v[i+1];
 
   ll ans = 0;
-  {
-    ll sum = 0;
-    ll tmp = 0;
-    rep(i, n) {
-      sum += v[i]-x[i];
-      tmp = max(tmp, sum);
+  for (ll a = 0; a <= n; ++a) {
+    for (ll b = n; b >= 0; --b) {
+      // a, bが原点でない限り、x[a] < x[b]を満たす必要がある
+      if (b != 0 and (x[a]>x[b])) continue;
+
+      // 距離は2OA+OB, OA+2OBのいずれか
+      ll oa = x[a];
+      ll ob = c - x[b];
+
+      // 栄養価を0~a, n~bまで求める
+      ll sum = 0;
+      rep(i, a) sum += v[i];
+      ll rev_sum = 0;
+      for (ll i = n; i >= b; --i) sum += v[i];
+
+      ll tmp = (sum-2*oa) + (rev_sum-ob);
+      tmp = max(tmp, (sum-oa) + (rev_sum-2*ob));
+      ans = max(ans, tmp);
     }
-    ans = max(ans, tmp);
-
-    sum = 0;
-    tmp = 0;
-    for (ll i = n-1; i >= 0; --i) {
-      sum += v[i] - (c-x[i]);
-      tmp = max(tmp, sum);
-    }
-    ans = max(ans, tmp);
-  }
-
-  // bを固定して最適なaを探す
-  for (ll b = n-1; b >= 1; --b) {
-    ll oa = max_value[b-1];
-    ll ob = (sum_v[n]-sum_v[b]) - (c-x[b]);
-
-    ll tmp = max(2*oa+ob, oa+2*ob);
-    ans = max(ans, tmp);
   }
 
   cout << ans << '\n';
