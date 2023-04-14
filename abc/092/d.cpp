8fb81357f9bfc4ca97fa7ce099a323ed109a1c8d
light <seseifunc16@gmail.com>
2023-04-14 17:30:25 +0900

    .

--- abc/092/d.cpp
+++ abc/092/d.cpp
@@ -13,31 +13,51 @@ int main() {
   vector<string> s(h, string(w, '.'));
   for (ll i = h/2; i < h; ++i) s[i] = string(w, '#');
 
+  const vector<pair<ll, ll>> dij = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+  auto check = [&](ll i, ll j) {
+    if (i < 0 or i >= h) return false;
+    if (j < 0 or j >= w) return false;
+    return true;
+  };
+
   // 上半分の白をb個黒に塗る
-  for (ll i = 0; i < h/2 - 1; i += 2) for (ll j = 0; j < w; j += 2) {
-    if (b > 0){
+  for (ll i = 0; i < h/2; ++i) for (ll j = 0; j < w; ++j) {
+    if (s[i][j] == '#') continue;
+
+    bool ok = true;
+    for (auto [di, dj] : dij) {
+      if (b == 0) continue;
+      ll ni = i+di; ll nj = j+dj;
+      if (not check(ni, nj)) continue;
+      if (s[ni][nj] == '#') ok = false;
+    }
+
+    if (ok and b > 0) {
       s[i][j] = '#';
       --b;
     }
-    if (b == 0) break;
   }
 
   // 下半分の黒をa個分白に塗る
-  for (ll i = h/2 + 1; i < h; i += 2) for (ll j = 0; j < w; j += 2) {
-    if (a > 0) {
+  for (ll i = h-1; i >= h/2; --i) for (ll j = 0; j < w; ++j) {
+    if (s[i][j] == '.') continue;
+    bool ok = true;
+
+    for (auto [di, dj] : dij) {
+      if (a == 0) continue;
+      ll ni = i+di; ll nj = j+dj;
+      if (not check(ni, nj)) continue;
+      if (s[ni][nj] == '.') ok = false;
+    }
+
+    if (ok and a > 0) {
       s[i][j] = '.';
       --a;
     }
-    if (a == 0) break;
   }
 
   cout << h << " " << w << '\n';
   for (string row : s) cout << row << '\n';
-
-  // debug
-  // rep(i, h) {
-  //   cout << i << ": ";
-  //   cout << s[i] << '\n';
-  // }
   return 0;
 }
