928f7038f70db8353ef85a49a4ffb5c5f9a82531
light <seseifunc16@gmail.com>
2023-04-18 14:03:15 +0900

    .

--- abc/296/e.cpp
+++ abc/296/e.cpp
@@ -5,6 +5,31 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<ll> indegree(n);
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    --a;
+    ++indegree[a];
+  }
+
+  queue<ll> que;
+  rep(i, n) {
+    if (indegree[i] == 0) que.emplace(i);
+  }
+
+  vector<ll> path;
+  while (not que.empty()) {
+    ll v = que.front();
+    que.pop();
+    path.emplace_back(v);
+    --indegree[v];
+    if (indegree[v] == 0) que.emplace(v);
+  }
+
+  cout << n-path.size() << '\n';
   return 0;
 }
