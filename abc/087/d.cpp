7f7f54da19b34d2b7d9de1ae296d71d24640e703
light <seseifunc16@gmail.com>
2023-04-23 12:16:09 +0900

    .

--- abc/087/d.cpp
+++ abc/087/d.cpp
@@ -5,6 +5,45 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n, m;
+  cin >> n >> m;
+
+  // <v, to, cost>
+  vector edge(n, vector<pair<ll, ll>>());
+  rep(i, m) {
+    ll l, r, d;
+    cin >> l >> r >> d;
+    --l; --r;
+    edge[l].emplace_back(r, d);
+    edge[r].emplace_back(l, -d);
+  }
+
+  vector<ll> x(n, -1);
+  vector<bool> seen(n);
+  rep(i, n) {
+    if (seen[i]) continue;
+    x[i] = 0;
+
+    auto dfs = [&](auto dfs, ll v) -> void {
+      seen[v] = true;
+
+      for (auto [to, d] : edge[v]) {
+        if (seen[to]) {
+          if (x[to]-x[v] != d) {
+            cout << "No" << '\n';
+            exit(0);
+          }
+        }
+        else {
+          x[to] = x[v] + d;
+          dfs(dfs, to);
+        }
+      }
+    };
+
+    dfs(dfs, i);
+  }
+
+  cout << "Yes" << '\n';
   return 0;
 }
