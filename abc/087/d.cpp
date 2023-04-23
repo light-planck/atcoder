46ea9b31419a8884bba37ad3706dc2e5651889b3
light <seseifunc16@gmail.com>
2023-04-23 12:34:53 +0900

    .

--- abc/087/d.cpp
+++ abc/087/d.cpp
@@ -4,44 +4,77 @@ using namespace std;
 using ll = long long;
 
 
+// https://qiita.com/drken/items/cce6fc5c579051e64fab
+template<class Abel> struct UnionFind {
+    vector<int> par;
+    vector<int> rank;
+    vector<Abel> diff_weight;
+
+    UnionFind(int n = 1, Abel SUM_UNITY = 0) {
+        init(n, SUM_UNITY);
+    }
+
+    void init(int n = 1, Abel SUM_UNITY = 0) {
+        par.resize(n); rank.resize(n); diff_weight.resize(n);
+        for (int i = 0; i < n; ++i) par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+    }
+
+    int root(int x) {
+        if (par[x] == x) {
+            return x;
+        }
+        else {
+            int r = root(par[x]);
+            diff_weight[x] += diff_weight[par[x]];
+            return par[x] = r;
+        }
+    }
+
+    Abel weight(int x) {
+        root(x);
+        return diff_weight[x];
+    }
+
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y, Abel w) {
+        w += weight(x); w -= weight(y);
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) swap(x, y), w = -w;
+        if (rank[x] == rank[y]) ++rank[x];
+        par[y] = x;
+        diff_weight[y] = w;
+        return true;
+    }
+
+    Abel diff(int x, int y) {
+        return weight(y) - weight(x);
+    }
+};
+
+
 int main() {
   ll n, m;
   cin >> n >> m;
 
-  // <v, to, cost>
-  vector edge(n, vector<pair<ll, ll>>());
+  UnionFind<ll> uf(n);
   rep(i, m) {
     ll l, r, d;
     cin >> l >> r >> d;
     --l; --r;
-    edge[l].emplace_back(r, d);
-    edge[r].emplace_back(l, -d);
-  }
 
-  vector<ll> x(n, -1);
-  vector<bool> seen(n);
-  rep(i, n) {
-    if (seen[i]) continue;
-    x[i] = 0;
-
-    auto dfs = [&](auto dfs, ll v) -> void {
-      seen[v] = true;
-
-      for (auto [to, d] : edge[v]) {
-        if (seen[to]) {
-          if (x[to]-x[v] != d) {
-            cout << "No" << '\n';
-            exit(0);
-          }
-        }
-        else {
-          x[to] = x[v] + d;
-          dfs(dfs, to);
-        }
+    if (uf.issame(l, r)) {
+      if (uf.diff(l, r) != d) {
+        cout << "No" << '\n';
+        return 0;
       }
-    };
-
-    dfs(dfs, i);
+    }
+    else {
+      uf.merge(l, r, d);
+    }
   }
 
   cout << "Yes" << '\n';
