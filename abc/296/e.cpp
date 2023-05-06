60d2232a18fb33c9bdb3b2f2c16cf0c7be084661
light <seseifunc16@gmail.com>
2023-05-06 13:14:12 +0900

    .

--- abc/296/e.cpp
+++ abc/296/e.cpp
@@ -2,30 +2,15 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
-#include <atcoder/scc>
-using namespace atcoder;
 
 
 int main() {
   ll n;
   cin >> n;
 
-  scc_graph graph(n);
-  ll ans = 0;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i], --a[i];
 
-  rep(i, n) {
-    ll a;
-    cin >> a;
-    --a;
-    graph.add_edge(i, a);
-    if (i == a) ++ans;
-  }
-
-  auto scc = graph.scc();
-  for (auto v : scc) {
-    if (v.size() >= 2) ans += v.size();
-  }
-
-  cout << ans << '\n';
+  
   return 0;
 }
