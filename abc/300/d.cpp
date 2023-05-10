ca60d9b4a9973650bc7027a01d4b9d8da81d8b4e
light <seseifunc16@gmail.com>
2023-05-10 20:55:19 +0900

    .

--- abc/300/d.cpp
+++ abc/300/d.cpp
@@ -11,35 +11,51 @@ using ll = long long;
 // https://drken1215.hatenablog.com/entry/2023/05/03/201800
 
 
+struct Sieve {
+  private:
+    int n_;
+    vector<bool> is_prime_;
+
+  public:
+    Sieve(int n) : n_(n), is_prime_(vector<bool>(n_+1, true)) {
+      is_prime_[0] = false;
+      is_prime_[1] = false;
+      for (long long i = 2; i*i <= n_; ++i) {
+        if (not is_prime_[i]) continue;
+        for (long long j = i*i; j <= n_; j += i) {
+          is_prime_[j]= false;
+        }
+      }
+    }
+
+    bool is_prime(int x) {
+      return is_prime_[x];
+    }
+};
+
+
 int main() {
   ll n;
   cin >> n;
 
-  ll MAX_P = 1000000;
+  const ll MAX_P = 1000000;
 
   // エラトステネス
-  vector<bool> is_prime(MAX_P, true);
-  is_prime[0] = is_prime[1] = false;
-  for (ll i = 2; i < MAX_P; ++i) {
-    if (not is_prime[i]) continue;
-    for (ll j = 2*i; j < MAX_P; j += i) {
-      is_prime[j] = false;
-    }
-  }
+  Sieve sieve(MAX_P);
 
   // 素数の累積和をとる
   vector<ll> cnt(MAX_P + 1);
   rep(i, MAX_P) {
-    if (is_prime[i]) cnt[i + 1] = cnt[i] + 1;
+    if (sieve.is_prime(i)) cnt[i + 1] = cnt[i] + 1;
     else cnt[i + 1] = cnt[i];
   }
 
   ll ans = 0;
   for (ll a = 1; a*a*a*a*a <= n; ++a) {
-    if (not is_prime[a]) continue;
+    if (not sieve.is_prime(a)) continue;
 
     for (ll b = a+1; b*b*b <= n; ++b) {
-      if (not is_prime[b]) continue;
+      if (not sieve.is_prime(b)) continue;
 
       ll MAX_C = sqrt(n / a / a / b);
 
