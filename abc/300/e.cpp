5281e0c114219438ca8fd363a647aa3cf4662fd4
light <seseifunc16@gmail.com>
2023-05-11 13:44:06 +0900

    .

--- abc/300/e.cpp
+++ abc/300/e.cpp
@@ -2,9 +2,38 @@
 #define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
 using namespace std;
 using ll = long long;
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<mint> dp(n+1);
+  dp[1] = 1;
+
+  mint div = (mint)1 / 6;
+
+  auto loop = [&]() {
+    while (true) {
+      vector<mint> prev(n+1);
+      swap(prev, dp);
+
+      for (ll j = 1; j <= n; ++j) {
+        if (prev[j] == 0) continue;
+
+        for (ll k = 1; k <= n; ++k) {
+          if (j*k > n) return;
+
+          dp[j*k] += prev[j] * div;
+        }
+      }
+    }
+  };
+  loop();
+
+  cout << dp[n].val() << '\n';
   return 0;
 }
