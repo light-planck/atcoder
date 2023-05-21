66a8b17088a34c1a939d8ce1e9ac0ac5e33d6737
light <seseifunc16@gmail.com>
2023-05-21 14:44:57 +0900

    .

--- abc/301/b.cpp
+++ abc/301/b.cpp
@@ -5,6 +5,20 @@ using ll = long long;
 
 
 int main() {
-  
+  ll n;
+  cin >> n;
+
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  rep(i, n-1) {
+    ll db = 1;
+    if (a[i+1]-a[i] < 0) db = -1;
+
+    for (ll b = a[i]; b != a[i+1]; b += db) {
+      cout << b << '\n';
+    }
+  }
+  cout << a.back() << '\n';
   return 0;
 }
