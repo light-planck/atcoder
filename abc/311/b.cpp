a0ea243755601ec6bc3b5825ecf3fb12ba278cba
light <55218925+light-planck@users.noreply.github.com>
2023-07-23 15:22:02 +0900

    .

--- abc/311/b.cpp
+++ abc/311/b.cpp
@@ -4,6 +4,26 @@ using namespace std;
 using ll = long long;
 
 int main() { 
-  
+  ll n, d;
+  cin >> n >> d;
+
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  ll ans = 0;
+  ll now = 0;
+  rep(j, d) {
+    bool ok = true;
+    rep(i, n) {
+      if (s[i][j] == 'x') ok = false;
+    }
+
+    if (ok) ++now;
+    else now = 0;
+
+    ans = max(ans, now);
+  }
+
+  cout << ans << '\n';
   return 0;
 }
