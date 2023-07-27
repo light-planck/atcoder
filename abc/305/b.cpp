30da12466411ceefe2c5b858b1705df1cb1bfc4f
light <55218925+light-planck@users.noreply.github.com>
2023-07-27 21:30:11 +0900

    .

--- abc/305/b.cpp
+++ abc/305/b.cpp
@@ -4,6 +4,13 @@ using namespace std;
 using ll = long long;
 
 int main() { 
-  
+  char p, q;
+  cin >> p >> q;
+
+  vector<ll> dist = {0, 3, 4, 8, 9, 14, 23};
+  ll i = p - 'A';
+  ll j = q - 'A';
+  if (i > j) swap(i, j);
+  cout << dist[j] - dist[i] << '\n';
   return 0;
 }
