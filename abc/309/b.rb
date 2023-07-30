dfbebc2f6e6d35c662685f3d4e340f4fad60c73b
light <55218925+light-planck@users.noreply.github.com>
2023-07-30 17:20:28 +0900

    .

--- abc/309/b.rb
+++ abc/309/b.rb
@@ -1,16 +1,18 @@
 N = gets.to_i
 A = Array.new(N) { gets.chomp.chars }
 
-B = A.dup
+B = Marshal.load(Marshal.dump(A))
 (0...N).each do |i|
   (0...N).each do |j|
     next unless i == 0 || i == N - 1 || j == 0 || j == N - 1
 
     B[i][j + 1] = A[i][j] if i == 0 && j + 1 < N
-    B[i + 1][j] = A[i][j] if j == N - 1
+    B[i + 1][j] = A[i][j] if i + 1 < N && j == N - 1
     B[i][j - 1] = A[i][j] if i == N - 1 && j > 0
-    B[i - 1][j] = A[i][j] if j == 0 && i - 1 >= 0
+    B[i - 1][j] = A[i][j] if i > 0 && j == 0
   end
 end
 
-puts B
+B.each do |b|
+  puts b.join
+end
