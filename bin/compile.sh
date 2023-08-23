bda305514bb69599668b5922b16bdecc7ec7e172
light <55218925+light-planck@users.noreply.github.com>
2023-08-23 18:30:24 +0900

    .

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,5 +2,5 @@
 
 file_name=$1
 
-g++-11 -std=c++17 -g -Wall -Wextra -fsanitize=address,undefined $file_name
+g++-11 -std=c++20 -g -Wall -Wextra -fsanitize=address,undefined $file_name
 clang-format -i $file_name
