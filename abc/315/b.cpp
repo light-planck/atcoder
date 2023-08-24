905d6bb9a8da89e6d866440a1fb93f97523e84f9
light <55218925+light-planck@users.noreply.github.com>
2023-08-24 20:56:06 +0900

    .

--- abc/315/b.cpp
+++ abc/315/b.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 using ll = long long;
 
-int main() { 
-  
+int main() {
+  ll M;
+  cin >> M;
+
+  vector<ll> D(M);
+  for (auto& d : D) cin >> d;
+
+  ll mid = (accumulate(D.begin(), D.end(), 0ll) + 1) / 2;
+  ll sum = 0;
+  rep(i, M) {
+    if (sum + D[i] >= mid) {
+      cout << i + 1 << " " << mid - sum << '\n';
+      break;
+    }
+    sum += D[i];
+  }
 }
