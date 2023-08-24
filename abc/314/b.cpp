e4a4d955b2968c2225d60ef1cdab1a3fd5992c69
light <55218925+light-planck@users.noreply.github.com>
2023-08-24 21:17:03 +0900

    .

--- abc/314/b.cpp
+++ abc/314/b.cpp
@@ -3,6 +3,45 @@
 using namespace std;
 using ll = long long;
 
-int main() { 
-  
+int main() {
+  ll N;
+  cin >> N;
+
+  vector A(N, set<ll>());
+  rep(i, N) {
+    ll C;
+    cin >> C;
+
+    rep(j, C) {
+      ll a;
+      cin >> a;
+      A[i].emplace(a);
+    }
+  }
+
+  ll X;
+  cin >> X;
+
+  vector<pair<ll, ll>> winner;
+  rep(i, N) {
+    if (A[i].count(X)) {
+      winner.emplace_back(A[i].size(), i + 1);
+    }
+  }
+  sort(winner.begin(), winner.end());
+
+  if (winner.size() == 0) {
+    cout << 0 << '\n';
+    exit(0);
+  }
+
+  ll min_count = winner[0].first;
+  vector<ll> ans;
+  for (auto [cnt, idx] : winner) {
+    if (cnt != min_count) break;
+    ans.emplace_back(idx);
+  }
+
+  cout << ans.size() << '\n';
+  for (auto x : ans) cout << x << '\n';
 }
