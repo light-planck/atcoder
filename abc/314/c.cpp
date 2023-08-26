e616ad9666a217e0926b82167d547c7535a4b021
light <55218925+light-planck@users.noreply.github.com>
2023-08-26 21:48:51 +0900

    .

--- abc/314/c.cpp
+++ abc/314/c.cpp
@@ -1,8 +1,37 @@
-#include <bits/stdc++.h>
-#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#include <deque>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 using namespace std;
-using ll = long long;
 
-int main() { 
-  
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  string S;
+  cin >> S;
+
+  vector<int> C(N);
+  for (auto& c : C) {
+    cin >> c;
+    --c;
+  }
+
+  map<int, deque<char>> deqs;
+  for (int i = 0; i < N; ++i) {
+    deqs[C[i]].emplace_back(S[i]);
+  }
+
+  for (auto& [_, deq] : deqs) {
+    char c = deq.back();
+    deq.pop_back();
+    deq.emplace_front(c);
+  }
+
+  for (int i = 0; i < N; ++i) {
+    cout << deqs[C[i]].front();
+    deqs[C[i]].pop_front();
+  }
+  cout << '\n';
 }
