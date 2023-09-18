9ca5cf661236a38c20d7c264bdf5e7309cd88357
light-planck <seseifunc16@gmail.com>
2023-09-18 16:43:17 +0900

    .

--- abc/319/b.cpp
+++ abc/319/b.cpp
@@ -4,5 +4,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  rep(i, N + 1) {
+    bool found = false;
+
+    for (ll j = 1; j <= 9; ++j) {
+      if (N % j == 0 and i % (N / j) == 0) {
+        cout << j;
+        found = true;
+        break;
+      }
+    }
+
+    if (not found) cout << '-';
+  }
+  cout << '\n';
 }
---
+++ bin/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+file_name=$1
+oj s --language 5001 $file_name
