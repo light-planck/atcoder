72f2ef4fb49abf10573f8dff15bebe16aa950264
light-planck <seseifunc16@gmail.com>
2023-09-19 08:38:34 +0900

    .

--- abc/306/c.cpp
+++ abc/306/c.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 using ll = long long;
 
-int main() { 
-  
+int main() {
+  ll N;
+  cin >> N;
+
+  vector<ll> A(3 * N);
+  for (auto& a : A) cin >> a;
+
+  vector idx(N, vector<ll>());
+  rep(i, 3 * N) { idx[A[i] - 1].emplace_back(i); }
+
+  vector<pair<ll, ll>> f;
+  rep(i, N) f.emplace_back(idx[i][1], i);
+  ranges::sort(f);
+
+  for (auto [_, i] : f) {
+    cout << i + 1 << '\n';
+  }
   return 0;
 }
