ebeca7d593f662833971963b2bd5c3f0fe791d4b
light-planck <seseifunc16@gmail.com>
2023-09-19 08:29:32 +0900

    .

--- abc/309/c.cpp
+++ abc/309/c.cpp
@@ -7,20 +7,27 @@ int main() {
   ll N, K;
   cin >> N >> K;
 
-  vector<pair<ll, ll>> decrease;
+  map<ll, ll> decrease;
   ll sum = 0;
   rep(i, N) {
     ll a, b;
     cin >> a >> b;
 
     sum += b;
-    decrease.emplace_back(a, b);
+    decrease[a] += b;
   }
-  ranges::sort(decrease);
 
-  ll ans = 1;
-  while (sum > K) {
-    
+  if (sum <= K) {
+    cout << 1 << '\n';
+    exit(0);
+  }
+
+  for (auto [day, dec] : decrease) {
+    sum -= dec;
+    if (sum <= K) {
+      cout << day + 1 << '\n';
+      break;
+    }
   }
   return 0;
 }
