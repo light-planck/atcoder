1b4b1faf1b7a2727c839f5c0ac470d2befc153d5
light-planck <seseifunc16@gmail.com>
2023-09-18 18:36:06 +0900

    .

--- abc/310/c.cpp
+++ abc/310/c.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 using ll = long long;
 
-int main() { 
-  
+int main() {
+  ll N;
+  cin >> N;
+
+  set<string> sticks;
+  rep(i, N) {
+    string s;
+    cin >> s;
+
+    string rev_s = s;
+    ranges::reverse(rev_s);
+    if (not sticks.contains(s) and not sticks.contains(rev_s)) {
+      sticks.emplace(s);
+    }
+  }
+
+  cout << ssize(sticks) << '\n';
   return 0;
 }
