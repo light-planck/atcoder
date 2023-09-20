c4baee9b08c4c9f5ef926437f756d68f2c1dd03f
light-planck <seseifunc16@gmail.com>
2023-09-20 21:56:42 +0900

    .

--- abc/303/c.cpp
+++ abc/303/c.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 using ll = long long;
 
-
 int main() {
   ll N, M, H, K;
   cin >> N >> M >> H >> K;
@@ -18,5 +17,45 @@ int main() {
     items.emplace(x, y);
   }
 
+  auto get_direction = [&](char c) -> pair<ll, ll> {
+    switch (c) {
+      case 'R':
+        return {1, 0};
+        break;
+      case 'L':
+        return {-1, 0};
+        break;
+      case 'U':
+        return {0, 1};
+        break;
+      case 'D':
+        return {0, -1};
+      default:
+        return {0, 0};
+        break;
+    }
+  };
 
+  ll x = 0;
+  ll y = 0;
+
+  rep(i, N) {
+    auto [dx, dy] = get_direction(S[i]);
+
+    --H;
+    x += dx;
+    y += dy;
+
+    if (H < K and items.contains({x, y})) {
+      H = K;
+      items.erase({x, y});
+    }
+
+    if (i < N - 1 and H == 0) {
+      cout << "No" << '\n';
+      exit(0);
+    }
+  }
+
+  cout << "Yes" << '\n';
 }
