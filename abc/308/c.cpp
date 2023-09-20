4c314d7f1aa150bbc11f8cc459957f9b4cf7a030
light-planck <seseifunc16@gmail.com>
2023-09-21 08:05:54 +0900

    .

--- abc/308/c.cpp
+++ abc/308/c.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 using ll = long long;
 
-int main() { 
-  
+int main() {
+  ll N;
+  cin >> N;
+
+  vector<long double> A(N), B(N);
+  rep(i, N) {
+    long double a, b;
+    cin >> a >> b;
+    A[i] = a;
+    B[i] = b;
+  }
+
+  vector<pair<long double, ll>> success;
+  rep(i, N) { success.emplace_back(A[i] / (A[i] + B[i]), -i); }
+  ranges::sort(success, ranges::greater());
+
+  for (auto [_, i] : success) {
+    cout << -i + 1 << '\n';
+  }
   return 0;
 }
