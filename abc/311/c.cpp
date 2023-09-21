cd6fd21eb9bec4a562f5d76c44774ad637a59131
light-planck <seseifunc16@gmail.com>
2023-09-22 02:52:14 +0900

    .

--- abc/311/c.cpp
+++ abc/311/c.cpp
@@ -14,4 +14,27 @@ int main() {
     --a;
     edge[i].emplace_back(a);
   }
+
+  vector<bool> seen(N);
+
+  auto dfs = [&](auto self, ll v, vector<ll> path) -> void {
+    if (ssize(path) > 1 and v == path[0]) {
+      cout << ssize(path) << '\n';
+      for (auto x : path) cout << x + 1 << '\n';
+      exit(0);
+    }
+
+    for (auto to : edge[v]) {
+      auto new_path = path;
+      path.emplace_back(to);
+      self(self, to, new_path);
+    }
+
+    path.pop_back();
+    seen[v] = true;
+  };
+
+  rep(i, N) {
+    if (not seen[i]) dfs(dfs, i, {i});
+  }
 }
--- bin/compile.sh
+++ bin/compile.sh
@@ -2,5 +2,5 @@
 
 file_name=$1
 
-g++-12 -std=c++20 -Wall -Wextra $file_name
+g++-13 -std=c++20 -Wall -Wextra $file_name
 clang-format -i $file_name
