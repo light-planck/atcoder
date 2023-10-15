6fa3b58b4d7778603b4ce1d0205ce834a3e90d56
light-planck <seseifunc16@gmail.com>
2023-10-15 18:35:38 +0900

    .

--- abc/322/b.cpp
+++ abc/322/b.cpp
@@ -4,5 +4,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N, M;
+  cin >> N >> M;
+
+  string S;
+  cin >> S;
+
+  string T;
+  cin >> T;
+
+  if (T.starts_with(S) and T.ends_with(S))
+    cout << 0 << '\n';
+  else if (T.starts_with(S))
+    cout << 1 << '\n';
+  else if (T.ends_with(S))
+    cout << 2 << '\n';
+  else
+    cout << 3 << '\n';
 }
