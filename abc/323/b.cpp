ccbf0066050ea3ad58658611c0549e80cc8d3774
light-planck <seseifunc16@gmail.com>
2023-10-15 19:07:33 +0900

    .

--- abc/323/b.cpp
+++ abc/323/b.cpp
@@ -4,5 +4,24 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  vector<string> S(N);
+  for (auto& s : S) cin >> s;
+
+  vector<ll> wins(N);
+  rep(i, N) {
+    rep(j, N) {
+      if (S[i][j] == 'o') {
+        ++wins[i];
+      }
+    }
+  }
+
+  vector<pair<ll, ll>> ans;
+  rep(i, N) { ans.emplace_back(wins[i], -(i + 1)); }
+  ranges::sort(ans, ranges::greater());
+
+  for (auto [_, i] : ans) cout << -i << '\n';
 }
