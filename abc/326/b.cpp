d6db25ccb9bcd3a315eb36eca7210fe4ad2c275b
light-planck <seseifunc16@gmail.com>
2023-11-11 19:26:22 +0900

    .

--- abc/326/b.cpp
+++ abc/326/b.cpp
@@ -4,5 +4,15 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  for (ll i = N; i < 1000; ++i) {
+    string S = to_string(i);
+
+    if ((S[0] - '0') * (S[1] - '0') == S[2] - '0') {
+      cout << i << '\n';
+      break;
+    }
+  }
 }
