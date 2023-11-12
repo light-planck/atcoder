f8e0e52170a35b03124ab89d936ddcc79f709448
light-planck <seseifunc16@gmail.com>
2023-11-12 10:28:39 +0900

    .

--- abc/328/b.cpp
+++ abc/328/b.cpp
@@ -4,5 +4,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  vector<ll> D(N);
+  for (auto& d : D) cin >> d;
+
+  ll ans = 0;
+  for (ll m = 1; m <= N; ++m) {
+    for (ll d = 1; d <= D[m - 1]; ++d) {
+      string s = to_string(m) + to_string(d);
+      set<char> st;
+      for (auto c : s) st.emplace(c);
+      if (ssize(st) == 1) ++ans;
+    }
+  }
+
+  cout << ans << '\n';
 }
