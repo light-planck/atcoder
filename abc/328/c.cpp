2842da2beaa1356d31ef67ee1673d7ce16da9a92
light-planck <seseifunc16@gmail.com>
2023-11-12 16:31:11 +0900

    .

--- abc/328/c.cpp
+++ abc/328/c.cpp
@@ -4,5 +4,24 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N, Q;
+  cin >> N >> Q;
+
+  string S;
+  cin >> S;
+
+  vector<ll> sum(N + 1);
+  rep(i, N) {
+    sum[i + 1] = sum[i];
+
+    if (S[i] == S[i + 1]) ++sum[i + 1];
+  }
+
+  while (Q--) {
+    ll l, r;
+    cin >> l >> r;
+    --l;
+    --r;
+    cout << sum[r] - sum[l] << '\n';
+  }
 }
