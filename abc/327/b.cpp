d07e4c4af2d4a3e9626cc26d944fe6fa85b3cfd2
light-planck <seseifunc16@gmail.com>
2023-11-12 16:43:35 +0900

    .

--- abc/327/b.cpp
+++ abc/327/b.cpp
@@ -4,5 +4,25 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll B;
+  cin >> B;
+
+  auto Pow = [](ll x, ll n) {
+    ll res = 1;
+    while (n > 0) {
+      if (n & 1) res *= x;
+      x *= x;
+      n >>= 1;
+    }
+    return res;
+  };
+
+  for (ll a = 1; Pow(a, a) <= B; ++a) {
+    if (Pow(a, a) == B) {
+      cout << a << '\n';
+      return 0;
+    }
+  }
+
+  cout << -1 << '\n';
 }
