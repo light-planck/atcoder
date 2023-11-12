eee98ca1dbf8d226cff7d70237d9a6550d29ac82
light-planck <seseifunc16@gmail.com>
2023-11-12 17:06:44 +0900

    .

--- abc/326/c.cpp
+++ abc/326/c.cpp
@@ -4,5 +4,18 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N, M;
+  cin >> N >> M;
+
+  vector<ll> A(N);
+  for (auto& a : A) cin >> a;
+  ranges::sort(A);
+
+  ll ans = 0;
+  rep(i, N) {
+    ll idx = ranges::lower_bound(A, M + A[i]) - A.begin();
+    ans = max(ans, idx - i);
+  }
+
+  cout << ans << '\n';
 }
