0cd6c78199b6240b33641141b3162883ac93b1ed
light-planck <seseifunc16@gmail.com>
2023-12-10 21:35:11 +0900

    .

---
+++ abc/329/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+using namespace std;
+using ll = long long;
+
+int main() {
+  string S;
+  cin >> S;
+
+  for (auto c : S) cout << c << ' ';
+  cout << '\n';
+}
