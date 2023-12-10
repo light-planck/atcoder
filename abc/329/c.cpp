70c012c8df791d1739ceb49127923a6dda950dd2
light-planck <seseifunc16@gmail.com>
2023-12-10 21:59:42 +0900

    .

--- abc/329/c.cpp
+++ abc/329/c.cpp
@@ -4,5 +4,30 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  string S;
+  cin >> S;
+
+  char now = '.';
+  ll length = 0;
+  map<char, ll> max_length;
+  for (auto c : S) {
+    if (c == now) {
+      ++length;
+    } else {
+      max_length[now] = max(max_length[now], length);
+      now = c;
+      length = 1;
+    }
+  }
+  max_length[now] = max(max_length[now], length);
+
+  ll ans = 0;
+  for (auto [_, length] : max_length) {
+    ans += length;
+  }
+
+  cout << ans << '\n';
 }
