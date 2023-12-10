802bd6b310e776e2cbef4be137628fe0fa4d42b6
light-planck <seseifunc16@gmail.com>
2023-12-10 21:39:36 +0900

    .

--- abc/329/b.cpp
+++ abc/329/b.cpp
@@ -4,5 +4,17 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  vector<ll> A(N);
+  for (auto &a : A) cin >> a;
+
+  ranges::sort(A, ranges::greater());
+  ll max_a = A[0];
+  for (auto a : A) {
+    if (a == max_a) continue;
+    cout << a << '\n';
+    exit(0);
+  }
 }
