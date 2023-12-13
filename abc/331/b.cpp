8a8f979c2232bc632d5215b69613cbc32237e3d7
light-planck <seseifunc16@gmail.com>
2023-12-13 12:31:46 +0900

    .

--- abc/331/b.cpp
+++ abc/331/b.cpp
@@ -4,5 +4,21 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N, S, M, L;
+  cin >> N >> S >> M >> L;
+
+  constexpr ll limit = 100;
+  ll ans = 1ll << 60;
+  for (ll s = 0; s < limit; ++s) {
+    for (ll m = 0; m < limit; ++m) {
+      for (ll l = 0; l < limit; ++l) {
+        ll count = 6 * s + 8 * m + 12 * l;
+        if (count >= N) {
+          ans = min(ans, S * s + M * m + L * l);
+        }
+      }
+    }
+  }
+
+  cout << ans << '\n';
 }
