93695583f4b98735a8468d7c1e717155aadf86eb
light-planck <seseifunc16@gmail.com>
2023-12-13 12:45:49 +0900

    .

--- abc/331/c.cpp
+++ abc/331/c.cpp
@@ -4,5 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  vector<ll> A(N);
+  rep(i, N) cin >> A[i];
+
+  auto b = A;
+  ranges::sort(b);
+
+  vector<ll> sum_b(N + 1);
+  rep(i, N) sum_b[i + 1] = sum_b[i] + b[i];
+
+  for (auto a : A) {
+    ll idx = ranges::upper_bound(b, a) - b.begin();
+    cout << sum_b[N] - sum_b[idx] << '\n';
+  }
 }
