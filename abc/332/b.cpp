f02137be8eb824508c0c191286492e0e29f37954
light-planck <seseifunc16@gmail.com>
2023-12-14 12:05:32 +0900

    .

--- abc/332/b.cpp
+++ abc/332/b.cpp
@@ -4,5 +4,25 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll K, G, M;
+  cin >> K >> G >> M;
+
+  ll g = 0;
+  ll m = 0;
+  ll cnt = 0;
+  while (cnt < K) {
+    if (g == G) {
+      g = 0;
+    } else if (m == 0) {
+      m = M;
+    } else {
+      ll delta = min(G - g, m);
+      g += delta;
+      m -= delta;
+    }
+
+    ++cnt;
+  }
+
+  cout << g << ' ' << m << '\n';
 }
