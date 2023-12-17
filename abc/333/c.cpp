ebb490a21d7c12fea5ee61a794de516df2692012
light-planck <seseifunc16@gmail.com>
2023-12-17 09:15:00 +0900

    .

--- abc/333/c.cpp
+++ abc/333/c.cpp
@@ -4,5 +4,20 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N;
+  cin >> N;
+
+  ll m = 12;
+  vector<ll> numbers;
+  rep(i, m) { numbers.emplace_back(stoll(string(i + 1, '1'))); }
+
+  vector<ll> sum;
+  rep(i, m) rep(j, m) rep(k, m) {
+    sum.emplace_back(numbers[i] + numbers[j] + numbers[k]);
+  }
+  ranges::sort(sum);
+  auto uq = ranges::unique(sum);
+  sum.erase(uq.begin(), uq.end());
+
+  cout << sum[N - 1] << '\n';
 }
