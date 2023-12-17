39ddb606ec94ef33d831cef918c121f7b26d5180
light-planck <seseifunc16@gmail.com>
2023-12-17 10:06:27 +0900

    .

--- abc/329/d.cpp
+++ abc/329/d.cpp
@@ -4,5 +4,25 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll N, M;
+  cin >> N >> M;
+
+  vector<ll> cnt(N);
+  ll max_a = 0;
+  ll idx = 0;
+  rep(i, M) {
+    ll a;
+    cin >> a;
+    --a;
+    ++cnt[a];
+
+    if (cnt[a] > max_a) {
+      max_a = cnt[a];
+      idx = a;
+    } else if (cnt[a] == max_a and a < idx) {
+      idx = a;
+    }
+
+    cout << idx + 1 << '\n';
+  }
 }
