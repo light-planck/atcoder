de32ac5164555a11633b8bf1f6b554cb4e8933d3
light-planck <seseifunc16@gmail.com>
2023-12-19 12:15:11 +0900

    .

--- abc/330/c.cpp
+++ abc/330/c.cpp
@@ -4,5 +4,23 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  
+  ll D;
+  cin >> D;
+
+  ll ans = 1ll << 60;
+  for (ll x = 0; x * x <= D; ++x) {
+    ll c = x * x - D;
+
+    if (c >= 0) {
+      ans = min(ans, c);
+      continue;
+    }
+
+    vector<ll> y = {(ll)sqrt(-c), (ll)ceil(sqrt(-c))};
+    for (auto yi : y) {
+      ans = min(ans, abs(c + yi * yi));
+    }
+  }
+
+  cout << ans << '\n';
 }
