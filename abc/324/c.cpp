76d1cecdced09beb13aabae12b4e77de20100eea
light-planck <seseifunc16@gmail.com>
2023-12-20 10:38:56 +0900

    .

--- abc/324/c.cpp
+++ abc/324/c.cpp
@@ -5,46 +5,59 @@ using ll = long long;
 
 int main() {
   ll N;
-  cin >> N;
-
   string T;
-  cin >> T;
+  cin >> N >> T;
 
-  ll m = T.size();
-
-  vector<string> left(m + 1);
-  left[0] = "";
-  rep(i, m) left[i + 1] = left[i] + T[i];
-
-  vector<string> right(m + 1);
-  right[0] = "";
-  rep(i, m) right[i + 1] = T[m - 1 - i] + right[i];
-
-  unordered_set<string> deleted;
-  unordered_set<string> add;
-  unordered_set<string> changed;
-  rep(i, m + 1) {
-    if (i < m) deleted.emplace(left[i] + right[m - i - 1]);
-
-    rep(j, 'z' - 'a' + 1) {
-      char c = char('a' + j);
-
-      add.emplace(left[i] + c + right[m - i]);
-      if (i < m) changed.emplace(left[i] + c + right[m - i - 1]);
-    }
-  }
-
-  vector<ll> ans;
+  set<ll> ans;
   rep(i, N) {
     string s;
     cin >> s;
 
-    if (s == T or add.contains(s) or deleted.contains(s) or
-        changed.contains(s)) {
-      ans.emplace_back(i + 1);
+    if (abs(ssize(s) - ssize(T)) > 1) continue;
+
+    bool found = false;
+    if (s == T) {
+      ans.emplace(i + 1);
+      found = true;
+    }
+
+    if (found) continue;
+    rep(j, ssize(s) + 1) {
+      rep(k, 'z' - 'a') {
+        string u = s.substr(0, j) + (char)('a' + k) + s.substr(j);
+
+        if (u == T) {
+          ans.emplace(i + 1);
+          found = true;
+        }
+      }
+    }
+
+    if (found) continue;
+    rep(j, ssize(s)) {
+      string u = s.substr(0, j) + s.substr(j + 1);
+
+      if (u == T) {
+        ans.emplace(i + 1);
+        found = true;
+      }
+    }
+
+    if (found) continue;
+    rep(j, ssize(s)) {
+      rep(k, 'z' - 'a') {
+        string u = s;
+        u[j] = 'a' + k;
+
+        if (u == T) {
+          ans.emplace(i + 1);
+        }
+      }
     }
   }
 
-  cout << ans.size() << '\n';
-  for (auto x : ans) cout << x << '\n';
+  cout << ssize(ans) << '\n';
+  for (auto x : ans) {
+    cout << x << '\n';
+  }
 }
