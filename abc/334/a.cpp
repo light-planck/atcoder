4e4821f1cc6303d279fdd0c7b39fe9e2faad75b5
light-planck <seseifunc16@gmail.com>
2023-12-28 12:45:07 +0900

    .

---
+++ abc/334/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll B, G;
+  cin >> B >> G;
+
+  if (B > G) cout << "Bat" << '\n';
+  else cout << "Glove" << '\n';
+}
