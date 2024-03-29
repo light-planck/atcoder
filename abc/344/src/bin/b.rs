5756a9b502d1cef17f821ae0e6ee75325fecd43b
light-planck <seseifunc16@gmail.com>
2024-03-30 01:48:52 +0900

    .

--- abc/344/src/bin/b.rs
+++ abc/344/src/bin/b.rs
@@ -1,6 +1,14 @@
-use proconio::{fastout, input};
+use std::io::{self, Read};
 
-#[fastout]
 fn main() {
-    input! {}
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let a = input
+        .lines()
+        .filter_map(|line| line.parse::<i32>().ok())
+        .collect::<Vec<i32>>();
+
+    for ai in a.iter().rev() {
+        println!("{}", ai);
+    }
 }
