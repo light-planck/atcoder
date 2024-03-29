35821abf2edd7cb1352e02032f0b16abf832b72c
light-planck <seseifunc16@gmail.com>
2024-03-30 01:26:01 +0900

    .

--- abc/345/src/bin/b.rs
+++ abc/345/src/bin/b.rs
@@ -3,6 +3,9 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        
+        x: i64,
     }
+
+    let ans = if x >= 0 { (x + 10 - 1) / 10 } else { x / 10 };
+    println!("{}", ans);
 }
