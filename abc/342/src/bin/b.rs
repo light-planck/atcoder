4cea3b395d01e8eca1ae66aad4532bc7de440b98
light-planck <seseifunc16@gmail.com>
2024-03-30 20:49:01 +0900

    .

--- abc/342/src/bin/b.rs
+++ abc/342/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        p:[i32; n],
+        q: i32,
+        ab: [(i32, i32); q],
+    }
+
+    for (a, b) in ab.into_iter() {
+        for p in &p {
+            if *p == a || *p == b {
+                println!("{}", *p);
+                break;
+            }
+        }
+    }
 }
