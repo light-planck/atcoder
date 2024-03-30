25213daea7b397ecf9266659172bebc126de3f5c
light-planck <seseifunc16@gmail.com>
2024-03-30 20:57:38 +0900

    .

--- abc/340/src/bin/b.rs
+++ abc/340/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        q: i32,
+        queries: [(i32, i32); q],
+    }
+
+    let mut a: Vec<i32> = vec![];
+    for (t, x) in queries {
+        if t == 1 {
+            a.push(x);
+        } else {
+            println!("{}", a[a.len() - x as usize]);
+        }
+    }
 }
