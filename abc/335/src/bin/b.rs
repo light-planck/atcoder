6969c3fcd0b3b05cc4efa51440691c6d26e99d0c
light-planck <seseifunc16@gmail.com>
2024-03-30 22:38:27 +0900

    .

--- abc/335/src/bin/b.rs
+++ abc/335/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+    }
+
+    for x in 0..n + 1 {
+        for y in 0..n + 1 {
+            for z in 0..n + 1 {
+                if x + y + z <= n {
+                    println!("{} {} {}", x, y, z);
+                }
+            }
+        }
+    }
 }
