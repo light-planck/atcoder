65827c53b532f5b3c3e6f0c2f24d9404f379b305
light-planck <seseifunc16@gmail.com>
2024-03-30 16:50:25 +0900

    .

--- abc/343/src/bin/b.rs
+++ abc/343/src/bin/b.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        a: [[i32; n]; n],
+    }
+
+    for row in a {
+        for (idx, &ai) in row.iter().enumerate() {
+            if ai == 1 {
+                print!("{} ", idx + 1);
+            }
+        }
+        println!();
+    }
 }
