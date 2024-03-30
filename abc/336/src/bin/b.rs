dd698bd34669dd50bd43d6bc25255ef6fbf27bee
light-planck <seseifunc16@gmail.com>
2024-03-30 22:33:16 +0900

    .

--- abc/336/src/bin/b.rs
+++ abc/336/src/bin/b.rs
@@ -2,5 +2,20 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+    }
+
+    let str = format!("{:b}", n);
+    let mut ans = 0;
+
+    for c in str.chars().rev() {
+        if c == '0' {
+            ans += 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
