1fd1378a07fac433a8390cc0aa0118c24ff67956
light-planck <seseifunc16@gmail.com>
2024-03-30 23:20:04 +0900

    .

--- abc/341/src/bin/b.rs
+++ abc/341/src/bin/b.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        mut a: [i32; n],
+        st: [(i32, i32); n-1],
+    }
+
+    for (i, (s, t)) in st.into_iter().enumerate() {
+        if a[i] >= s {
+            a[i + 1] += t * (a[i] / s);
+            a[i] -= s * (a[i] / s);
+        }
+    }
+
+    println!("{}", a.last().unwrap());
 }
