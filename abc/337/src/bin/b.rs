5db79d11547d533dbf719e15b6a6b55c7691b301
light-planck <seseifunc16@gmail.com>
2024-03-30 22:28:45 +0900

    .

--- abc/337/src/bin/b.rs
+++ abc/337/src/bin/b.rs
@@ -1,6 +1,17 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    let mut t = s.clone();
+    t.sort();
+
+    if s == t {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
