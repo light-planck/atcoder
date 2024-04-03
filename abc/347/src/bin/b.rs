c42f8ca5a2fcd5d9cde2b7dee1931fbc6454104a
light-planck <seseifunc16@gmail.com>
2024-04-03 09:05:44 +0900

    .

--- abc/347/src/bin/b.rs
+++ abc/347/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::{fastout, input};
+use im_rc::HashSet;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-        
+        s: Chars,
     }
+
+    let mut hash: HashSet<String> = HashSet::new();
+    let n = s.len();
+
+    for l in 0..n {
+        for r in l + 1..n + 1 {
+            let str = s[l..r].iter().collect::<String>();
+            hash.insert(str);
+        }
+    }
+
+    println!("{}", hash.len());
 }
