330c1aedf519318c31164c3369c2a34599e12ac1
light-planck <seseifunc16@gmail.com>
2024-04-08 23:16:39 +0900

    .

--- abc/343/src/bin/c.rs
+++ abc/343/src/bin/c.rs
@@ -2,5 +2,23 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i64,
+    }
+
+    let mut ans = 1;
+
+    for i in 1..1e6 as i64 {
+        let x = i * i * i;
+
+        if x > n {
+            break;
+        }
+
+        if x.to_string() == x.to_string().chars().rev().collect::<String>() {
+            ans = x;
+        }
+    }
+
+    println!("{}", ans);
 }
