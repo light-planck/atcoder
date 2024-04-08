da5c3d557835c4a8a774346312c197e8cfb29089
light-planck <seseifunc16@gmail.com>
2024-04-08 21:54:36 +0900

    .

--- abc/346/src/bin/c.rs
+++ abc/346/src/bin/c.rs
@@ -1,6 +1,23 @@
+use im_rc::HashSet;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i64,
+        k: i64,
+        a: [i64; n],
+    }
+
+    let mut st: HashSet<i64> = HashSet::new();
+    for a in a {
+        if 1 <= a && a <= k {
+            st.insert(a);
+        }
+    }
+
+    let sub = st.into_iter().sum::<i64>();
+    let ans = k * (k + 1) / 2 - sub;
+
+    println!("{}", ans);
 }
