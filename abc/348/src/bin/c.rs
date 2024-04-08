5c27efb07b99ab80044d76e66ce61b29759b23c6
light-planck <seseifunc16@gmail.com>
2024-04-08 21:33:48 +0900

    .

--- abc/348/src/bin/c.rs
+++ abc/348/src/bin/c.rs
@@ -1,8 +1,19 @@
+use im_rc::HashMap;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        
+        n: i64,
+        ac: [(i64, i64); n],
     }
+
+    let mut min_a: HashMap<i64, i64> = HashMap::new();
+    for (a, c) in ac {
+        let entry = min_a.entry(c).or_insert(a);
+        *entry = (*entry).min(a);
+    }
+
+    let ans = min_a.values().max().unwrap();
+    println!("{}", ans);
 }
