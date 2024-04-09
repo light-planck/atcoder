6ceb665a77368bfedf995cda76cca1bc3fa9ad9d
light-planck <seseifunc16@gmail.com>
2024-04-09 22:29:24 +0900

    .

--- abc/339/src/bin/c.rs
+++ abc/339/src/bin/c.rs
@@ -7,17 +7,14 @@ fn main() {
         a: [i64; n],
     }
 
-    let mut initial_value = 0 as i64;
     let mut now = 0;
     for a in &a {
         if now + a < 0 {
-            initial_value = initial_value.max(-(now + a));
             now = 0;
         } else {
             now += a;
         }
     }
 
-    let ans = a.into_iter().sum::<i64>() + initial_value;
-    println!("{}", ans);
+    println!("{}", now);
 }
