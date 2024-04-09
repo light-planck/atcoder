b72a59d30a39a00bf7d531e8da8efea41f568aae
light-planck <seseifunc16@gmail.com>
2024-04-10 00:25:43 +0900

    .

--- abc/337/src/bin/c.rs
+++ abc/337/src/bin/c.rs
@@ -1,6 +1,40 @@
+use std::collections::VecDeque;
+
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut edge: Vec<Vec<usize>> = vec![vec![]; n];
+    let mut start = 0;
+    for (i, a) in a.into_iter().enumerate() {
+        if a == -1 {
+            start = i;
+        } else {
+            edge[(a - 1) as usize].push(i);
+        }
+    }
+
+    let mut b: Vec<usize> = vec![];
+    let mut deq = VecDeque::new();
+    deq.push_back(start);
+
+    while let Some(v) = deq.pop_front() {
+        b.push(v + 1);
+
+        for to in &edge[v] {
+            deq.push_back(*to);
+        }
+    }
+
+    let ans = b
+        .into_iter()
+        .map(|v| v.to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+    println!("{}", ans);
 }
