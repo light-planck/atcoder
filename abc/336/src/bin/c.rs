7a01cf774ea4e1e38a4aa065df26d5b29b8db33e
light-planck <seseifunc16@gmail.com>
2024-04-10 21:39:26 +0900

    .

--- abc/336/src/bin/c.rs
+++ abc/336/src/bin/c.rs
@@ -2,5 +2,24 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        mut n: i64,
+    }
+    n -= 1;
+
+    let mut digits = Vec::new();
+    while n >= 0 {
+        digits.push(n % 5);
+        n /= 5;
+
+        if n == 0 {
+            break;
+        }
+    }
+    digits.reverse();
+
+    for digit in digits {
+        print!("{}", digit * 2);
+    }
+    println!();
 }
