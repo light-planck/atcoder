3f59fcd671ef38e677b1f246e4707ecd41c54226
light-planck <seseifunc16@gmail.com>
2024-04-13 00:09:25 +0900

    .

--- abc/345/src/bin/c.rs
+++ abc/345/src/bin/c.rs
@@ -1,6 +1,25 @@
-use proconio::{fastout, input};
+use im_rc::HashMap;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    let mut ans = 0;
+    let mut cnt: HashMap<char, i64> = HashMap::new();
+    for (j, c) in s.iter().enumerate() {
+        let count = cnt.entry(*c).or_insert(0);
+        ans += j as i64 - *count;
+        *count += 1;
+    }
+
+    if let Some(max_cnt) = cnt.values().max() {
+        if max_cnt > &1 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
