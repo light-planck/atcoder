96f0db1d83513425a1e0314fc7f722b5206913d3
light-planck <seseifunc16@gmail.com>
2024-04-15 20:46:28 +0900

    .

--- abc/349/src/bin/b.rs
+++ abc/349/src/bin/b.rs
@@ -1,8 +1,29 @@
-use proconio::{fastout, input};
+use im_rc::HashMap;
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-        
+        s: Chars,
     }
+
+    let mut cnt: HashMap<char, i64> = HashMap::new();
+    for c in &s {
+        *cnt.entry(*c).or_insert(0) += 1;
+    }
+
+    let mut types: HashMap<i64, Vec<char>> = HashMap::new();
+    for (c, n) in cnt {
+        let entry = types.entry(n).or_insert(Vec::new());
+        entry.push(c);
+    }
+
+    for (_, cs) in types {
+        if cs.len() == 0 || cs.len() != 2 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
