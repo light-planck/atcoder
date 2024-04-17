7f0143ae655e7a26c1d7d885af7709c2fb1ba392
light-planck <seseifunc16@gmail.com>
2024-04-17 23:10:08 +0900

    .

--- abc/349/src/bin/c.rs
+++ abc/349/src/bin/c.rs
@@ -1,8 +1,25 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-        
+        s: Chars,
+        t: Chars,
     }
+
+    let mut ti = 0;
+    let max_ti = if t[2] == 'X' { t.len() - 1 } else { t.len() };
+
+    for c in s {
+        if c.to_uppercase().to_string() == t[ti].to_string() {
+            ti += 1;
+
+            if ti == max_ti {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
