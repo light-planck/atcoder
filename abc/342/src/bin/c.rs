ea8b878c0b67f758c51dd04ab5c47a98175b1897
light-planck <seseifunc16@gmail.com>
2024-04-17 23:56:54 +0900

    .

--- abc/342/src/bin/c.rs
+++ abc/342/src/bin/c.rs
@@ -4,33 +4,28 @@ use proconio::{fastout, input, marker::Chars};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        _: usize,
         s: Chars,
         q: usize,
         cd: [(char, char); q],
     }
 
-    let mut edge: Vec<Vec<usize>> = vec![vec![]; 26];
-    for (c, d) in cd {
-        let c = c as usize - 'a' as usize;
-        let d = d as usize - 'a' as usize;
-        edge[c].push(d);
+    let mut converter: HashMap<char, char> = HashMap::new();
+    for c in 'a'..='z' {
+        converter.insert(c, c);
     }
 
-    let mut converter: HashMap<char, char> = HashMap::new();
-    for i in 0..26 {
-        if edge[i].is_empty() {
-            continue;
-        }
-
-        let mut stack = vec![i];
-        while let Some(j) = stack.pop() {
-            for &k in &edge[j] {
-                if !converter.contains_key(&(k as u8 as char)) {
-                    converter.insert(k as u8 as char, j as u8 as char);
-                    stack.push(k);
-                }
+    for (c, d) in cd {
+        for (k, v) in converter.clone() {
+            if v == c {
+                converter.insert(k, d);
             }
         }
     }
+
+    let ans = s
+        .into_iter()
+        .map(|c| converter.get(&c).unwrap())
+        .collect::<String>();
+    println!("{}", ans);
 }
