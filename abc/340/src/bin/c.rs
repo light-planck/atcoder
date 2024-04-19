e8732ff5a7e5ecc706294f53676ae612c56a7c24
light-planck <seseifunc16@gmail.com>
2024-04-20 00:19:59 +0900

    .

--- abc/340/src/bin/c.rs
+++ abc/340/src/bin/c.rs
@@ -1,6 +1,27 @@
+use im_rc::HashMap;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: i64,
+    }
+
+    let mut memo: HashMap<i64, i64> = HashMap::new();
+    let ans = f(n, &mut memo);
+    println!("{}", ans);
+}
+
+fn f(x: i64, memo: &mut HashMap<i64, i64>) -> i64 {
+    if x == 1 {
+        return 0;
+    }
+
+    if memo.contains_key(&x) {
+        return memo[&x];
+    }
+
+    let res = x + f(x / 2, memo) + f((x + 2 - 1) / 2, memo);
+    memo.insert(x, res);
+    res
 }
