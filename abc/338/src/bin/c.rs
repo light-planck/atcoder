4f2a85d18f9fe238181f640f4d84e9e0ca654632
light-planck <seseifunc16@gmail.com>
2024-04-20 11:01:52 +0900

    .

--- abc/338/src/bin/c.rs
+++ abc/338/src/bin/c.rs
@@ -2,5 +2,38 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: [i64; n],
+        a: [i64; n],
+        b: [i64; n],
+    }
+
+    let mut ans = 0;
+
+    for a_cnt in 0..(1e6 as i64 + 1) {
+        let rem_q = (0..n).map(|i| q[i] - a[i] * a_cnt).collect::<Vec<i64>>();
+        if rem_q.iter().any(|&x| x < 0) {
+            break;
+        }
+
+        let mut ok = 0;
+        let mut ng = 1e6 as i64 + 1;
+        while (ok - ng).abs() > 1 {
+            let mid = (ok + ng) / 2;
+            let can_cook = (0..n)
+                .map(|i| q[i] - a[i] * a_cnt - b[i] * mid)
+                .all(|x| x >= 0);
+
+            if can_cook {
+                ok = mid;
+            } else {
+                ng = mid;
+            }
+        }
+
+        ans = ans.max(a_cnt + ok);
+    }
+
+    println!("{}", ans);
 }
