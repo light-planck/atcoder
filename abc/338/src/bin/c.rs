89fdcb9700c210df505a2d0a2f3daa15b20966cf
light-planck <seseifunc16@gmail.com>
2024-04-20 11:14:10 +0900

    .

--- abc/338/src/bin/c.rs
+++ abc/338/src/bin/c.rs
@@ -9,15 +9,13 @@ fn main() {
         b: [i64; n],
     }
 
-    let binary_search = |a_cnt: i64| -> i64 {
+    let binary_search = |rem_q: &Vec<i64>| -> i64 {
         let mut ok = 0;
         let mut ng = 1e6 as i64 + 1;
 
         while (ok - ng).abs() > 1 {
             let mid = (ok + ng) / 2;
-            let can_cook = (0..n)
-                .map(|i| q[i] - a[i] * a_cnt - b[i] * mid)
-                .all(|x| x >= 0);
+            let can_cook = (0..n).map(|i| rem_q[i] - b[i] * mid).all(|x| x >= 0);
 
             if can_cook {
                 ok = mid;
@@ -32,12 +30,16 @@ fn main() {
     let mut ans = 0;
 
     for a_cnt in 0..(1e6 as i64 + 1) {
-        let rem_q = (0..n).map(|i| q[i] - a[i] * a_cnt).collect::<Vec<i64>>();
+        let rem_q = q
+            .iter()
+            .zip(a.iter())
+            .map(|(&qi, &ai)| qi - ai * a_cnt)
+            .collect::<Vec<i64>>();
         if rem_q.iter().any(|&x| x < 0) {
             break;
         }
 
-        let ok = binary_search(a_cnt);
+        let ok = binary_search(&rem_q);
         ans = ans.max(a_cnt + ok);
     }
 
