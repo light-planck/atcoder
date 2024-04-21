cfc8d48d0e525b79b11ec84204057e57a1e6f57a
light-planck <seseifunc16@gmail.com>
2024-04-21 17:21:19 +0900

    .

--- abc/350/src/bin/c.rs
+++ abc/350/src/bin/c.rs
@@ -1,8 +1,41 @@
+use im_rc::HashMap;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        
+        n: usize,
+        mut a: [i64; n],
+    }
+
+    let mut pos: HashMap<i64, usize> = HashMap::from(
+        a.iter()
+            .enumerate()
+            .map(|(i, &x)| (x, i))
+            .collect::<Vec<_>>(),
+    );
+    let mut ans: Vec<(usize, usize)> = vec![];
+
+    for target in 1..=n as i64 {
+        let mut i = *pos.get(&target).unwrap();
+        let mut j = (target - 1) as usize;
+
+        if i == j {
+            continue;
+        }
+
+        if i >= j {
+            (i, j) = (j, i);
+        }
+
+        ans.push((i + 1, j + 1));
+        a.swap(i, j);
+        pos.insert(a[i], i);
+        pos.insert(a[j], j);
+    }
+
+    println!("{}", ans.len());
+    for (i, j) in ans {
+        println!("{} {}", i, j);
     }
 }
