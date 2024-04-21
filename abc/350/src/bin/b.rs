f90fb377056bbda014d0b49e15a7bc1052a36036
light-planck <seseifunc16@gmail.com>
2024-04-21 13:10:23 +0900

    .

--- abc/350/src/bin/b.rs
+++ abc/350/src/bin/b.rs
@@ -3,6 +3,16 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        
+        (n, q): (usize, usize),
+        t: [i64; q],
     }
+
+    let mut teeth = vec![true; n];
+
+    for i in t {
+        teeth[(i - 1) as usize] = !teeth[(i - 1) as usize];
+    }
+
+    let ans = teeth.into_iter().filter(|&i| i).count();
+    println!("{}", ans);
 }
