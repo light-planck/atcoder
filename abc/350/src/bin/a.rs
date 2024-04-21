3be0978654d138ff4af6feb04816c3b0db5cfa74
light-planck <seseifunc16@gmail.com>
2024-04-21 12:08:48 +0900

    .

--- abc/350/src/bin/a.rs
+++ abc/350/src/bin/a.rs
@@ -3,6 +3,13 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        
+        s: String,
+    }
+
+    let number = s[3..].parse::<i32>().unwrap();
+    if 0 < number && number < 350 && number != 316 {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
