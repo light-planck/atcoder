33f69e3e2e0ff3589f574f60b8d7c010b4d2a323
light-planck <seseifunc16@gmail.com>
2024-04-25 21:40:15 +0900

    .

--- abc/335/src/bin/d.rs
+++ abc/335/src/bin/d.rs
@@ -1,6 +1,60 @@
 use proconio::{fastout, input};
 
+struct Pos {
+    i: usize,
+    j: usize,
+}
+
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    let mut s: Vec<Vec<i64>> = vec![vec![-1; n]; n];
+    let mut dir = 0;
+    let dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    let mut seen: i64 = 1;
+    let mut pos = Pos { i: 0, j: 0 };
+
+    while seen < n as i64 * n as i64 {
+        let Pos { i, j } = pos;
+        s[i][j] = seen;
+
+        loop {
+            let (di, dj) = dirs[dir];
+            let (ni, nj) = (i as i64 + di, j as i64 + dj);
+
+            if ni < 0
+                || nj < 0
+                || ni >= n as i64
+                || nj >= n as i64
+                || s[ni as usize][nj as usize] != -1
+            {
+                dir = (dir + 1) % 4;
+            } else {
+                pos = Pos {
+                    i: ni as usize,
+                    j: nj as usize,
+                };
+                break;
+            }
+        }
+
+        seen += 1;
+    }
+
+    for row in &s {
+        println!(
+            "{}",
+            row.iter()
+                .map(|&x| if x != -1 {
+                    x.to_string()
+                } else {
+                    "T".to_string()
+                })
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+    }
 }
