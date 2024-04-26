fe76c0426db84d52dd971658f1441bc62c7e005f
light-planck <seseifunc16@gmail.com>
2024-04-26 21:26:13 +0900

    .

--- abc/334/src/bin/d.rs
+++ abc/334/src/bin/d.rs
@@ -3,6 +3,22 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        
+        (n, q): (usize, usize),
+        mut r: [i64; n],
+        queries: [i64; q],
+    }
+
+    r.sort_unstable();
+    let cusum: Vec<i64> = r
+        .iter()
+        .scan(0, |acc, &x| {
+            *acc += x;
+            Some(*acc)
+        })
+        .collect();
+
+    for query in queries.into_iter() {
+        let idx = cusum.partition_point(|x| x <= &query);
+        println!("{}", idx);
     }
 }
