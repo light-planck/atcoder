ec16ba11b5c8d5c6ea1f8d666c2d5efef56fcfc3
light-planck <seseifunc16@gmail.com>
2024-04-27 00:38:07 +0900

    .

--- abc/328/src/bin/d.rs
+++ abc/328/src/bin/d.rs
@@ -1,8 +1,26 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
-        
+        s: Chars,
+    }
+
+    let mut stk: Vec<char> = vec![];
+    for c in s {
+        stk.push(c);
+
+        if stk.len() >= 3 && c == 'C' {
+            let last = &stk[stk.len() - 3..];
+            if last == ['A', 'B', 'C'] {
+                for _ in 0..3 {
+                    stk.pop();
+                }
+            }
+        }
+    }
+
+    if !stk.is_empty() {
+        println!("{}", stk.into_iter().collect::<String>());
     }
 }
