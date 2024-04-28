6aefde4ba5abb99ab3c8d4909ba1521897f2d9b5
light-planck <seseifunc16@gmail.com>
2024-04-28 11:04:22 +0900

    .

--- abc/351/src/bin/a.rs
+++ abc/351/src/bin/a.rs
@@ -2,6 +2,10 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        a: [i64; 9],
+        b: [i64; 8],
     }
+
+    let ans = (a.into_iter().sum::<i64>() - b.into_iter().sum::<i64>() + 1).max(0);
+    println!("{}", ans);
 }
