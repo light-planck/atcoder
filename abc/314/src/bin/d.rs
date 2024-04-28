5094f43ea52c8d383a8ec3c62af59ab09390e3a3
light-planck <seseifunc16@gmail.com>
2024-04-28 10:52:14 +0900

    .

--- abc/314/src/bin/d.rs
+++ abc/314/src/bin/d.rs
@@ -1,5 +1,31 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        mut s: Chars,
+        q: usize,
+        queries: [(usize, usize, char); q],
+    }
+
+    // 最後に大文字か小文字に変換したクエリのインデックスを取得する
+    let last_convert_query_idx = if let Some(idx) = queries.iter().rposition(|&(t, _, _)| t > 1) {
+        idx
+    } else {
+        0
+    };
+
+    for (i, (t, x, c)) in queries.into_iter().enumerate() {
+        if t == 1 {
+            s[x - 1] = c;
+        } else if i == last_convert_query_idx {
+            match t {
+                2 => s = s.iter().map(|c| c.to_ascii_lowercase()).collect(),
+                3 => s = s.iter().map(|c| c.to_ascii_uppercase()).collect(),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    println!("{}", s.iter().collect::<String>());
 }
