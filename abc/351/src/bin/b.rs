40b78d1b86f518dfc53f90ae0a2bbfa543782178
light-planck <seseifunc16@gmail.com>
2024-04-28 15:59:51 +0900

    .

--- abc/351/src/bin/b.rs
+++ abc/351/src/bin/b.rs
@@ -1,7 +1,17 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        
+        n: usize,
+        a: [Chars; n],
+        b: [Chars; n],
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            if a[i][j] != b[i][j] {
+                println!("{} {}", i + 1, j + 1);
+            }
+        }
     }
 }
