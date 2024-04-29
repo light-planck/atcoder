1e5ec8f07c8d275df35f1fe91a350909270cc43b
light-planck <seseifunc16@gmail.com>
2024-04-29 12:25:12 +0900

    .

--- abc/351/src/bin/c.rs
+++ abc/351/src/bin/c.rs
@@ -2,6 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        n: usize,
+        a: [i64; n],
     }
+
+    let mut b: Vec<i64> = Vec::new();
+
+    for a in &a {
+        b.push(*a);
+
+        while b.len() > 1 && b[b.len() - 1] == b[b.len() - 2] {
+            let c = b.pop().unwrap();
+            let d = b.pop().unwrap();
+
+            if c != d {
+                b.push(c);
+                b.push(d);
+                break;
+            }
+
+            b.push(c + 1);
+        }
+    }
+
+    println!("{}", b.len());
 }
