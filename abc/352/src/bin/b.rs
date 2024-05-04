14504bcb497efd3b408003478c74da140020d1b3
light-planck <seseifunc16@gmail.com>
2024-05-04 23:52:25 +0900

    .

--- abc/352/src/bin/b.rs
+++ abc/352/src/bin/b.rs
@@ -1,7 +1,20 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        
+        s: Chars,
+        t: Chars,
     }
+
+    let mut si = 0;
+    let mut ans = vec![];
+    for (i, c) in t.into_iter().enumerate() {
+        if s[si] == c {
+            si += 1;
+            ans.push(i + 1);
+        }
+    }
+
+    println!("{}", ans.iter().join(" "));
 }
