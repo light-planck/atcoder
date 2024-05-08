2a99bb4c3f2ed420e913ecce9568ba81c4da45af
light-planck <seseifunc16@gmail.com>
2024-05-08 12:32:43 +0900

    .

--- abc/352/src/bin/c.rs
+++ abc/352/src/bin/c.rs
@@ -2,6 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        n: usize,
+        ab: [(i64, i64); n],
     }
+
+    let mut ans = 0;
+    let a_sum = ab.iter().map(|(a, _)| a).sum::<i64>();
+    for (a, b) in ab {
+        ans = ans.max(a_sum - a + b);
+    }
+
+    println!("{}", ans);
 }
