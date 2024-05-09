784a2c45b4c7465562edd5fba36d37e4d74dd266
light-planck <seseifunc16@gmail.com>
2024-05-09 12:11:07 +0900

    .

--- abc/307/src/bin/d.rs
+++ abc/307/src/bin/d.rs
@@ -1,7 +1,35 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        
+        _: usize,
+        s: Chars,
+    }
+
+    let mut stack = Vec::new();
+    let mut left_count = 0;
+
+    for c in s {
+        if c == '(' {
+            stack.push(c);
+            left_count += 1;
+        } else if c == ')' {
+            if left_count > 0 {
+                while let Some(d) = stack.pop() {
+                    if d == '(' {
+                        left_count -= 1;
+                        break;
+                    }
+                }
+            } else {
+                stack.push(c);
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+
+    if !stack.is_empty() {
+        println!("{}", stack.into_iter().collect::<String>());
     }
 }
