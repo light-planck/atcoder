a449dd955b29686f7ee772e6fe21da2deade1011
light-planck <seseifunc16@gmail.com>
2024-05-12 12:26:17 +0900

    .

--- abc/353/src/bin/b.rs
+++ abc/353/src/bin/b.rs
@@ -2,6 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        (n, k): (usize, usize),
+        a: [usize; n],
     }
+
+    let mut ans = 0;
+    let mut now = 0;
+    for a in a {
+        if a + now <= k {
+            now += a;
+        } else {
+            now = a;
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans + 1);
 }
