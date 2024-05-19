20a7bb498fd82179813d31babf0ae1f66ad3a066
light-planck <seseifunc16@gmail.com>
2024-05-19 15:35:18 +0900

    .

--- abc/354/src/bin/b.rs
+++ abc/354/src/bin/b.rs
@@ -2,6 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        n: usize,
+        sc: [(String, i64); n]
     }
+
+    let t = sc.iter().map(|(_, c)| c).sum::<i64>();
+    let mut s = sc.iter().map(|(s, _)| s).collect::<Vec<&String>>();
+    s.sort_unstable();
+
+    let idx = (t as usize) % n;
+    println!("{}", s[idx]);
 }
