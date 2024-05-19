dd59a1f0563c98d3ad12826aa7981ac2b13dc45b
light-planck <seseifunc16@gmail.com>
2024-05-19 15:28:10 +0900

    .

--- abc/354/src/bin/a.rs
+++ abc/354/src/bin/a.rs
@@ -2,6 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        h: i64,
+    }
+
+    let mut now: i64 = 0;
+    for day in 0.. {
+        if now > h {
+            println!("{}", day);
+            break;
+        }
+
+        now += 1 << day;
     }
 }
