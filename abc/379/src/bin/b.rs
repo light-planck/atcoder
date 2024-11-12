834e4ab2d5f0b01c6bbbf51d6557a4b32f47729a
light-planck <seseifunc16@gmail.com>
2024-11-13 01:43:15 +0900

    .

--- abc/379/src/bin/b.rs
+++ abc/379/src/bin/b.rs
@@ -1,7 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        
+        n: usize,
+        k: usize,
+        s: Chars,
     }
+
+    let mut ans = 0;
+    let mut i = 0;
+
+    while i < n {
+        let mut j = i;
+
+        while j < n && s[j] != 'X' {
+            j += 1;
+        }
+
+        if j - i >= k {
+            ans += (j - i) / k;
+        }
+
+        i = j + 1;
+    }
+
+    println!("{}", ans);
 }
